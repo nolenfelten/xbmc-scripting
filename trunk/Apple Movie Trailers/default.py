badec54a99790dbe3fe6efcf720515c0670698d5
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2006-09-23 21:33:40 +0000

    added: credit and version information

--- trunk/Apple Movie Trailers/default.py
+++ trunk/Apple Movie Trailers/default.py
@@ -3,6 +3,12 @@ sys.path.append( os.path.join( sys.path[0], 'lib' ) )
 
 import gui
 
+__scriptname__ = 'Apple Movie Trailers'
+__author__ = 'Apple Movie Trailers (AMT) Team'
+__url__ = 'http://code.google.com/p/xbmc-scripting/'
+__credits__ = 'XBMC TEAM, efnet/#xbmc-scripting'
+__version__ = '1.0 pre-release'
+
 if __name__ == '__main__':
     ui = gui.GUI()
     if ui.SUCCEEDED:
