2bb45bdfc2b382901329171f45270247b5752bcf
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2006-10-01 06:42:45 +0000

    moved: AMT release tags to their own directory

---
+++ 0.9/lib/guisettings.py
@@ -0,0 +1,211 @@
+import xbmc, xbmcgui
+import os, guibuilder
+import amt_util, default
+
+class GUI( xbmcgui.WindowDialog ):
+    def __init__( self ):
+        self.settings = amt_util.getSettings()
+        self.setupGUI()
+        if ( not self.SUCCEEDED ): self.close()
+        else:
+            self.skin = self.settings['skin']
+            self.setupConstants()
+            self.setControlsValues()
+            ##remove disabled when update script routine is done
+            self.controls['Update Button']['control'].setEnabled( False )
+
+    def setupGUI(self):
+        skinPath = os.path.join( os.getcwd(), 'skins' ).replace( ';', '' ) # workaround apparent xbmc bug - os.getcwd() returns an extraneous semicolon (;) at the end of the path
+        self.skinPath = os.path.join( skinPath, self.settings['skin'] )
+        self.imagePath = os.path.join( self.skinPath, 'gfx' )
+        res = self.getResolution()
+        if ( res == 0 or res % 2 ): skin = 'settings_16x9.xml'
+        else: skin = 'settings.xml'
+        if ( not os.path.isfile( os.path.join( self.skinPath, skin ) ) ): skin = 'settings.xml'
+        guibuilder.GUIBuilder( self, os.path.join( self.skinPath, skin ), self.imagePath, useDescAsKey=True, fastMethod=True, debug=False )
+
+    def setupConstants( self ):
+        self.controllerAction = amt_util.setControllerAction()
+        self.quality = amt_util.setQuality()
+        self.mode = amt_util.setMode()
+        self.startup = amt_util.setStartupCategory()
+        self.thumbnail = amt_util.setThumbnailDisplay()
+        
+    def setControlsValues( self ):
+        self.controls['Trailer Quality Button']['control'].setLabel( 'Trailer Quality: [%s]' % ( self.quality[self.settings['trailer quality']], ) )
+        self.controls['Mode Button']['control'].setLabel( 'Mode: [%s]' % ( self.mode[self.settings['mode']], ) )
+        self.controls['Save Folder Button']['control'].setLabel( 'Save Folder: [%s]' % ( self.settings['save folder'], ) )
+        self.controls['Save Folder Button']['control'].setEnabled( self.settings['mode'] == 2 )
+        self.controls['Skin Button']['control'].setLabel( 'Skin: [%s]' % ( self.settings['skin'], ) )
+        self.controls['Startup Category Button']['control'].setLabel( 'Startup Category: [%s]' % ( self.startup[self.settings['startup category']], ) )
+        self.controls['Thumbnail Display Button']['control'].setLabel( 'Thumbnail Display: [%s]' % ( self.thumbnail[self.settings['thumbnail display']], ) )
+
+    def saveSettings( self ):
+        ret = amt_util.saveSettings( self.settings )
+        if ( not ret ):
+            dialog = xbmcgui.Dialog()
+            ok = dialog.ok( default.__scriptname__, 'There was an error saving your settings.' )
+        else:
+            if ( self.skin != self.settings['skin']):
+                dialog = xbmcgui.Dialog()
+                ok = dialog.ok( default.__scriptname__, "The skin change won't take affect until you restart." )
+            self.closeDialog()
+
+    def toggleTrailerQuality( self ):
+        tq = self.settings['trailer quality'] + 1
+        if ( tq > 2 ): tq = 0
+        self.settings['trailer quality'] = tq
+        #self.controls['Trailer Quality Button']['control'].setLabel( 'Trailer Quality: [%s]' % ( self.quality[tq], ) )
+        self.setControlsValues()
+      
+    def toggleMode( self ):
+        m = self.settings['mode'] + 1
+        if ( m > 2 ): m = 0
+        self.settings['mode'] = m
+        #self.controls['Mode Button']['control'].setLabel( 'Mode: [%s]' % ( self.mode[m], ) )
+        self.setControlsValues()
+
+    def toggleStartupCategory( self ):
+        c = self.settings['startup category'] + 1
+        if ( c > 2 ): c = 0
+        self.settings['startup category'] = c
+        #self.controls['Startup Category Button'].setLabel( 'Startup Category: [%s]' % ( self.startup[c], ) )
+        self.setControlsValues()
+        
+    def toggleThumbnailDisplay( self ):
+        t = self.settings['thumbnail display'] + 1
+        if ( t > 2 ): t = 0
+        self.settings['thumbnail display'] = t
+        #self.controls['Startup Category Button'].setLabel( 'Startup Category: [%s]' % ( self.startup[c], ) )
+        self.setControlsValues()
+    
+    def browseForFolder( self ):
+        dialog = xbmcgui.Dialog()
+        folder = dialog.browse( 0, 'a save folder', 'files' )
+        if ( folder ):
+            self.settings['save folder'] = folder
+            #self.controls['Save Folder Button']['control'].setLabel( 'Save Folder: [%s]' % ( folder, ) )
+            self.setControlsValues()
+
+    def chooseSkin( self ):
+        skinPath = os.path.join( os.getcwd(), 'skins' ).replace( ';', '' ) # workaround apparent xbmc bug - os.getcwd() returns an extraneous semicolon (;) at the end of the path
+        skins = os.listdir( skinPath )
+        skins.sort()
+        self.showPopup( 'choose your skin', skins )
+        
+    def getThumb( self, choice ):
+        thumbnail = os.path.join( os.getcwd(), 'skins', choice.getLabel(), 'thumbnail.tbn' ).replace( ';', '' ) # workaround apparent xbmc bug - os.getcwd() returns an extraneous semicolon (;) at the end of the path
+        if ( os.path.isfile( thumbnail ) ):
+            self.controls['Popup Thumb']['control'].setImage( thumbnail )
+        else:
+            self.controls['Popup Thumb']['control'].setImage( '' )
+        
+    def hidePopup( self ):
+        self.setPopupVisibility( False )
+        self.setFocus( self.controls['Skin Button']['control'] )
+    
+    def showPopup( self, title, items ):
+        self.controls['Popup Label']['control'].setLabel( title )
+        self.controls['Popup List']['control'].reset()
+        for item in items:
+            if ( os.path.isfile( os.path.join( os.getcwd(), 'skins', item, 'skin.xml' ).replace( ';', '' ) ) and
+                os.path.isfile( os.path.join( os.getcwd(), 'skins', item, 'settings.xml' ).replace( ';', '' ) ) ):
+                self.controls['Popup List']['control'].addItem( item )
+        self.setPopupVisibility( True )
+        self.setFocus( self.controls['Popup List']['control'] )
+        
+    def setPopupVisibility( self, visible ):
+        self.controls['Popup Image']['control'].setVisible( visible )
+        self.controls['Popup Thumb']['control'].setVisible( visible )
+        self.controls['Popup Label']['control'].setVisible( visible )
+        self.controls['Popup List']['control'].setVisible( visible )
+        
+    def setSkinSelection( self ):
+        self.settings['skin'] = self.controls['Popup List']['control'].getSelectedItem().getLabel()
+        #self.controls['Skin Button']['control'].setLabel( 'Skin: [%s]' % (self.settings['skin'], ) )
+        self.setControlsValues()
+        self.hidePopup()
+    
+    
+    def setButtonNavigation( self, control ):
+        pass
+    #    self.controls['Ok Button']['control'].controlUp( control )
+    #    self.controls['Cancel Button']['control'].controlUp( control )
+    #    self.controls['Update Button']['control'].controlUp( control )
+    #    self.controls['Credits Button']['control'].controlUp( control )
+    #    self.controls['Ok Button']['control'].controlDown( control )
+    #    self.controls['Cancel Button']['control'].controlDown( control )
+    #    self.controls['Update Button']['control'].controlDown( control )
+    #    self.controls['Credits Button']['control'].controlDown( control )
+
+    def showCredits( self ):
+        try:
+            self.controls['Credits Label']['control'].setLabel( default.__scriptname__ )
+            self.controls['Version Label']['control'].setLabel( 'Version: %s' % ( default.__version__, ) )
+            self.controls['Team Credits Label']['control'].setLabel( 'Team Credits:' )
+            self.controls['Team Credits List']['control'].reset()
+            l = xbmcgui.ListItem( default.__credits_l1__, default.__credits_r1__ )
+            self.controls['Team Credits List']['control'].addItem( l )
+            l = xbmcgui.ListItem( default.__credits_l2__, default.__credits_r2__ )
+            self.controls['Team Credits List']['control'].addItem( l )
+            l = xbmcgui.ListItem( default.__credits_l3__, default.__credits_r3__ )
+            self.controls['Team Credits List']['control'].addItem( l )
+            self.setCreditsVisibility( True )
+            self.setFocus( self.controls['Skin Credits List']['control'] )
+        except: print 'Credits Removed'
+            
+    def hideCredits( self ):
+        self.setCreditsVisibility( False )
+        self.setFocus( self.controls['Credits Button']['control'] )
+
+    def setCreditsVisibility( self, visible ):
+        try:
+            self.controls['Credits Image']['control'].setVisible( visible )
+            self.controls['Credits Label']['control'].setVisible( visible )
+            self.controls['Version Label']['control'].setVisible( visible )
+            self.controls['Team Credits Label']['control'].setVisible( visible )
+            self.controls['Team Credits List']['control'].setVisible( visible )
+            self.controls['Skin Credits Label']['control'].setVisible( visible )
+            self.controls['Skin Credits List']['control'].setVisible( visible )
+        except: print 'Credits Removed'
+            
+    def closeDialog( self ):
+        self.close()
+        
+    def onControl( self, control ):
+        if ( control is self.controls['Cancel Button']['control'] ):
+            self.closeDialog()
+        elif ( control is self.controls['Ok Button']['control'] ):
+            self.saveSettings()
+        elif ( control is self.controls['Credits Button']['control'] ):
+            self.showCredits()
+        elif ( control is self.controls['Trailer Quality Button']['control'] ):
+            self.toggleTrailerQuality()
+        elif ( control is self.controls['Mode Button']['control'] ):
+            self.toggleMode()
+        elif ( control is self.controls['Save Folder Button']['control'] ):
+            self.browseForFolder()
+        elif ( control is self.controls['Skin Button']['control'] ):
+            self.chooseSkin()
+        elif ( control is self.controls['Startup Category Button']['control'] ):
+            self.toggleStartupCategory()
+        elif ( control is self.controls['Thumbnail Display Button']['control'] ):
+            self.toggleThumbnailDisplay()
+        elif ( control is self.controls['Popup List']['control'] ):
+            self.setSkinSelection()
+            
+    def onAction( self, action ):
+        control = self.getFocus()
+        buttonDesc = self.controllerAction.get( action.getButtonCode(), 'n/a' )
+        if ( buttonDesc == 'B Button' or buttonDesc == 'Remote Back Button' ):
+            if ( control == self.controls['Popup List']['control'] ): self.hidePopup()
+            elif ( control == self.controls['Skin Credits List']['control'] ): self.hideCredits()
+            else: self.close()
+        elif ( buttonDesc == 'Back Button' or buttonDesc == 'Remote Menu Button' ):
+            if ( control == self.controls['Popup List']['control'] ): self.setSkinSelection()
+            elif ( control == self.controls['Skin Credits List']['control'] ): self.hideCredits()
+            else: self.saveSettings()
+        elif ( control == self.controls['Popup List']['control'] ):
+            self.getThumb( control.getSelectedItem() )
+        #elif ( control != self.controls['Ok Button']['control'] and control != self.controls['Cancel Button']['control'] ):
+        #    self.setButtonNavigation( control )
---
+++ 0.91/lib/gui.py
@@ -0,0 +1,282 @@
+'''
+Main GUI for Apple Movie Trailers
+'''
+__line1__       = 'Setting up script:'
+__line2__       = 'Importing modules & initializing...'
+
+def createProgressDialog( __line3__ ):
+    global dialog, pct
+    pct = 0
+    dialog = xbmcgui.DialogProgress()
+    dialog.create( default.__scriptname__ )
+    updateProgressDialog( __line3__ )
+
+def updateProgressDialog( __line3__ ):
+    global dialog, pct
+    pct += 5
+    dialog.update( pct, __line1__, __line2__, __line3__ )
+
+def closeProgessDialog():
+    global dialog
+    dialog.close()
+    
+import xbmcgui, default
+createProgressDialog( 'modules: xbmcgui, default' )
+
+updateProgressDialog( 'modules: xbmc, sys, os, traceback' )
+import xbmc, sys, os, traceback
+
+updateProgressDialog( 'modules: trailers, threading' )
+import trailers, threading
+
+updateProgressDialog( 'modules: guibuilder, guisettings' )
+import guibuilder, guisettings
+
+updateProgressDialog( 'modules: amt_util, cacheurl, shutil' )
+import amt_util, cacheurl, shutil
+
+
+class GUI( xbmcgui.Window ):
+    def __init__( self ):
+        self.getSettings()
+        self.setupGUI()
+        if ( not self.SUCCEEDED ): self.close()
+        else:
+            try:
+                closeProgessDialog()
+                ## enable when ready
+                self.controls['Search Button']['control'].setEnabled( False )
+                self.setupConstants()
+                self.trailers = trailers.Trailers()
+                if ( self.checkForDB() ):
+                    self.getGenreCategories()
+                    self.setStartupCategory()
+                else:
+                    dialog = xbmcgui.Dialog()
+                    ok = dialog.ok(default.__scriptname__, 'No database found!', 'You must Update all genre and movie information.')
+            except: 
+                xbmc.output('Error at script start')
+                self.exitScript()
+                
+    def setupGUI(self):
+        skinPath = os.path.join( os.getcwd(), 'skins' ).replace( ';', '' ) # workaround apparent xbmc bug - os.getcwd() returns an extraneous semicolon (;) at the end of the path
+        self.skinPath = os.path.join( skinPath, self.settings['skin'] )
+        self.imagePath = os.path.join( self.skinPath, 'gfx' )
+        res = self.getResolution()
+        if ( res == 0 or res % 2 ): skin = 'skin_16x9.xml'
+        else: skin = 'skin.xml'
+        if ( not os.path.isfile( os.path.join( self.skinPath, skin ) ) ): skin = 'skin.xml'
+        guibuilder.GUIBuilder( self, os.path.join( self.skinPath, skin ), self.imagePath, useDescAsKey=True, title=default.__scriptname__, line1=__line1__, dlg=dialog, pct=pct, debug=False )
+
+    def getSettings( self ):
+        self.settings = amt_util.getSettings()
+
+    def checkForDB( self ):
+        if ( os.path.isfile( os.path.join( os.getcwd(), 'data', 'AMT.pk' ).replace( ';', '' ) ) ): return True
+        else: return False
+
+    def setStartupCategory( self ):
+        startup = amt_util.setStartupCategory()
+        self.setGenre( startup[self.settings['startup category']] )
+        self.setListNavigation( '%s Button' % ( startup[self.settings['startup category']], ) )
+        if ( startup[self.settings['startup category']] != 'Genre' ):
+            self.getTrailerInfo( self.controls['Trailer List']['control'].getSelectedItem() )
+            
+    def setupConstants( self ):
+        # self.Timer is currently used for the Player() subclass so when an onPlayback* event occurs, it's instant.
+        self.Timer = threading.Timer(60*60, self.exitScript,() )
+        self.Timer.start()
+        self.MyPlayer = MyPlayer(xbmc.PLAYER_CORE_MPLAYER, function=self.myPlayerChanged)
+        self.controllerAction = amt_util.setControllerAction()
+        self.updateMethod = 0
+        self.thumbnail = None
+        self.genre = None
+    
+    # this function is used for skins like XBMC360 that have player information on screen
+    def myPlayerChanged(self):
+        self.controls['X Button On']['control'].setVisible( xbmc.getCondVisibility( self.controls['X Button On']['visible'] ) )
+        self.controls['X Button Off']['control'].setVisible( xbmc.getCondVisibility( self.controls['X Button Off']['visible'] ) )
+        self.controls['Full-Screen Visualisation Label']['control'].setVisible( xbmc.getCondVisibility( self.controls['Full-Screen Visualisation Label']['visible'] ) )
+        self.controls['Full-Screen Video Label']['control'].setVisible( xbmc.getCondVisibility( self.controls['Full-Screen Video Label']['visible'] ) )
+        
+    def playTrailer( self, title ):
+        trailer_urls = self.trailers.get_video_list( self.genre, title )
+        if ( self.settings['trailer quality'] >= len( trailer_urls ) ):
+            choice = len( trailer_urls ) - 1
+        else:
+            choice = self.settings['trailer quality']
+        try:
+            if ( self.settings['mode'] == 0 ):
+                filename = trailer_urls[choice].replace( '//', '/' ).replace( '/', '//', 1 )
+            elif ( self.settings['mode'] == 1):
+                url = trailer_urls[choice].replace( '//', '/' ).replace( '/', '//', 1 )
+                fetcher = cacheurl.HTTPProgressSave()
+                filename = fetcher.urlretrieve( url )
+            elif ( self.settings['mode'] == 2):
+                url = trailer_urls[choice].replace( '//', '/' ).replace( '/', '//', 1 )
+                fetcher = cacheurl.HTTPProgressSave( self.settings['save folder'] )
+                filename = fetcher.urlretrieve( url )
+                if ( filename ): self.saveThumbnail( filename )
+            if ( filename ): self.MyPlayer.play( filename )
+        except:
+            xbmc.output('ERROR: playing %s' % ( title, ) )
+            
+    def saveThumbnail( self, filename ):
+        try: 
+            newfilename = '%s.tbn' % ( os.path.splitext( filename )[0], )
+            if ( not os.path.isfile( newfilename ) ):
+                shutil.copyfile(self.thumbnail, newfilename )
+        except: pass
+
+    def showTrailerInfo( self, title ):
+        self.thumbnail, description = self.trailers.get_trailer_info( self.genre, title )
+        # Trailer Thumbnail
+        self.controls['Trailer Thumbnail']['control'].setImage( self.thumbnail )
+        # Trailer Title
+        self.controls['Trailer Title']['control'].setLabel( title )
+        # Trailer Description
+        self.controls['Trailer Info']['control'].reset()
+        if description:
+            self.controls['Trailer Info']['control'].setText( description )
+
+    def showTrailers( self, genre ):
+        self.controls['Trailer List']['control'].reset()
+        if ( genre == 'Newest' ): titles = self.trailers.get_newest_list()
+        elif ( genre == 'Exclusives' ): titles = self.trailers.get_exclusives_list()
+        else: titles = self.trailers.get_trailer_list( genre )
+        for title in titles: # now fill the list control
+            thumbnail, description = self.trailers.get_trailer_info( genre, title )
+            if ( self.settings['thumbnail display'] == 2 ): thumbnail = ''
+            elif ( self.settings['thumbnail display'] == 1 ): thumbnail = os.path.join( self.imagePath, 'generic-thumbnail.tbn' )
+            l = xbmcgui.ListItem( title, '', thumbnail )
+            self.controls['Trailer List']['control'].addItem( l )
+            
+    def updateDatabase( self ):
+        if (self.updateMethod == 0 ):
+            self.trailers.update_all()
+            #self.getGenreCategories()
+            #self.showTrailers( self.genre )
+            self.setGenre( self. genre )
+            
+    def getGenreCategories( self ):
+        self.controls['Genre List']['control'].reset()
+        for genre in self.trailers.get_genre_list():
+            thumbnail = os.path.join( self.imagePath, '%s.tbn' % ( genre, ) )
+            if ( not os.path.isfile( thumbnail ) ):
+                thumbnail = os.path.join( self.imagePath, 'genre-thumbnail.tbn' )
+            l = xbmcgui.ListItem( genre, '',  thumbnail)
+            self.controls['Genre List']['control'].addItem( l )
+
+    def exitScript(self):
+        if ( self.Timer ): self.Timer.cancel()
+        self.trailers.cleanup()
+        self.close()
+    
+    def showList( self, Genre ):
+        self.controls['Genre List']['control'].setVisible( Genre )
+        self.controls['Genre List']['control'].setEnabled( Genre )
+        self.controls['Trailer List']['control'].setVisible( not Genre )
+        self.controls['Trailer List']['control'].setEnabled( not Genre )
+        ## until initial visibilty is solved just hardcode them
+        #self.controls['Trailer Thumbnail']['control'].setVisible(xbmc.getCondVisibility( self.controls['Trailer Thumbnail']['visible'] ) )
+        #self.controls['Trailer Title']['control'].setVisible(xbmc.getCondVisibility( self.controls['Trailer Title']['visible'] ) )
+        #self.controls['Trailer Info']['control'].setVisible(xbmc.getCondVisibility( self.controls['Trailer Info']['visible'] ) )
+        self.controls['Trailer Backdrop']['control'].setVisible( not Genre )
+        self.controls['Trailer Thumbnail']['control'].setVisible( not Genre )
+        self.controls['Trailer Title']['control'].setVisible( not Genre )
+        self.controls['Trailer Info']['control'].setVisible( not Genre )
+        self.controls['Trailer Info']['control'].setEnabled( not Genre )
+        self.setCategoryLabel( self.genre )
+        if ( Genre ): self.setFocus(self.controls['Genre List']['control'] )
+        else: self.setFocus(self.controls['Trailer List']['control'] )
+        
+    def setGenre( self, genre ):
+        self.genre = genre
+        if ( genre != 'Genre' ): self.showTrailers( genre )
+        self.showList( genre == 'Genre' )
+
+    def setCategoryLabel( self, category ):
+        self.controls['Category Label']['control'].setLabel( category )
+            
+    def setListNavigation( self, button ):
+        self.controls['Trailer List']['control'].controlLeft( self.controls[button]['control'] )
+        self.controls['Trailer Info']['control'].controlRight( self.controls[button]['control'] )
+        
+    def getTrailerInfo( self, choice ):
+        title = choice.getLabel()
+        self.showTrailerInfo( title )
+    
+    def getTrailer( self, choice ):
+        title = choice.getLabel()
+        self.playTrailer( title )
+        
+    def getTrailerGenre( self, choice ):
+        self.genre = choice.getLabel()
+        self.showTrailers( self.genre )
+        self.setCategoryLabel( self.genre )
+        self.showList( False )
+    
+    def onControl( self, control ):
+        try:
+            if ( control is self.controls['Newest Button']['control'] ):
+                self.setGenre( 'Newest' )
+            elif ( control is self.controls['Exclusives Button']['control'] ):
+                self.setGenre( 'Exclusives' )
+            elif ( control is self.controls['Genre Button']['control'] ):
+                self.setGenre( 'Genre' )
+            elif ( control is self.controls['Update Button']['control'] ):
+                self.updateDatabase()
+            elif ( control is self.controls['Settings Button']['control'] ):
+                settings = guisettings.GUI()
+                settings.doModal()
+                #del settings
+                self.getSettings()
+            elif ( control is self.controls['Genre List']['control'] ):
+                self.getTrailerGenre( control.getSelectedItem() )
+            elif ( control is self.controls['Trailer List']['control'] ):
+                self.getTrailer( control.getSelectedItem() )
+        except: traceback.print_exc()
+            
+    def onAction( self, action ):
+        try:
+            buttonDesc = self.controllerAction.get(action.getButtonCode(), 'n/a')
+            if ( buttonDesc == 'Back Button' or buttonDesc == 'Remote Menu Button' ): self.exitScript()
+            elif (buttonDesc == 'B Button' or buttonDesc == 'Remote Back Button' ):
+                if ( self.genre != 'Newest' and self.genre != 'Exclusives' and self.genre != 'Genre'):
+                    self.setGenre( 'Genre' )
+            else:
+                control = self.getFocus()
+                if ( control is self.controls['Trailer List']['control'] ):
+                    self.getTrailerInfo( control.getSelectedItem() )
+                elif ( control is self.controls['Exclusives Button']['control'] ):
+                    self.setListNavigation('Exclusives Button')
+                elif ( control is self.controls['Newest Button']['control'] ):
+                    self.setListNavigation('Newest Button')
+                elif ( control is self.controls['Genre Button']['control'] ):
+                    self.setListNavigation('Genre Button')
+                elif ( control is self.controls['Search Button']['control'] ):
+                    self.setListNavigation('Search Button')
+                elif ( control is self.controls['Settings Button']['control'] ):
+                    self.setListNavigation('Settings Button')
+                elif ( control is self.controls['Update Button']['control'] ):
+                    self.setListNavigation('Update Button')
+        except: traceback.print_exc()
+
+
+
+## Thanks Thor918 for this class ##
+class MyPlayer(xbmc.Player):
+    def  __init__(self, *args, **kwargs):
+        if (kwargs.has_key('function')): 
+            self.function = kwargs['function']
+            xbmc.Player.__init__(self)
+    
+    def onPlayBackStopped(self):
+        self.function()
+    
+    def onPlayBackEnded(self):
+        self.function()
+    
+    def onPlayBackStarted(self):
+        self.function()
+    
