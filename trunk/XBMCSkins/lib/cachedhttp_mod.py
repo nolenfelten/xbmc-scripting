34c6c8e72a9c51c6fcca6617cb8384adbfdf731c
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2006-10-01 15:16:20 +0000

    initial commit: added all XBMCSkins files

---
+++ trunk/XBMCSkins/lib/cachedhttp_mod.py
@@ -0,0 +1,10 @@
+"""
+    This is a simple script that lets me keep track of which version of 
+    cachedhttp I'm using.
+"""
+CACHEDHTTP_DIR = 'cachedhttp-v13'
+
+import sys, os
+path = os.path.join( os.path.dirname( sys.modules['cachedhttp_mod'].__file__ ), CACHEDHTTP_DIR )
+sys.path.insert( 0, path )
+from cachedhttp import *
