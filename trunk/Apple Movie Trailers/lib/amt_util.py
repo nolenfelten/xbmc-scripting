72435cabcf3a2df2bd360c59747efc1b0ee43b80
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2006-10-12 17:36:27 +0000

    added: english lanuguage strings (82, 218) changed: strings (202-204) added: playlist button changed: order of some buttons added: you can now specify any genre for startup category changed: saved settings, (Please delete old settings.txt file) added: delete saved trailer to context menu changed: defaults for poster, thumbs and saved is now 'None' (This is what was being returned after DB creation) fixed: Blank poster now shows for trailers without one.

--- trunk/Apple Movie Trailers/lib/amt_util.py
+++ trunk/Apple Movie Trailers/lib/amt_util.py
@@ -4,10 +4,10 @@ def setThumbnailDisplay( _ ):
     return [_(310), _(311), _(312)]
 
 def setStartupCategory( _ ):
-    return [_(200), _(201), _(202)]
+    return [_(201), _(200)]#, _(202)]
 
 def setStartupCategoryActual():
-    return ['Newest', 'Exclusives', 'Genre']
+    return ['Exclusives', 'Newest', 'Genre']
 
 def setQuality( _ ):
     return [_(320), _(321), _(322)]
@@ -51,9 +51,8 @@ class Settings:
             self.mode = int( s[1] )
             self.skin = s[2]
             self.save_folder = s[3]
-            self.startup_category = int( s[4] )
-            self.startup_category_id = int( s[5] )
-            self.thumbnail_display = int( s[6] )
+            self.startup_category_id = int( s[4] )
+            self.thumbnail_display = int( s[5] )
         except:
             self.setDefaults()
 
@@ -62,17 +61,15 @@ class Settings:
         self.mode = 0
         self.skin = 'Default'
         self.save_folder = 'f:\\'
-        self.startup_category = 0
         self.startup_category_id = 1
         self.thumbnail_display = 1
 
     def saveSettings( self ):
         try:
-            strSettings = '%d|%d|%s|%s|%d|%d|%d' % ( 
+            strSettings = '%d|%d|%s|%s|%d|%d' % ( 
                 self.trailer_quality,
                 self.mode, self.skin,
                 self.save_folder,
-                self.startup_category,
                 self.startup_category_id,
                 self.thumbnail_display, )
             f = open( os.path.join( os.getcwd(), 'data', 'settings.txt' ).replace( ';', '' ), 'w' )
