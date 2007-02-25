6a33ee29c72426e7df01ad29daf534669815d803
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-02-25 13:46:21 +0000

    changed: [XBMC Changelog] sys.path[0] -> os.getcwd(";","") changed: [XBMC Changelog] renamed /extras/ -> /resources/

--- trunk/XBMC Changelog/default.py
+++ trunk/XBMC Changelog/default.py
@@ -8,26 +8,30 @@
 #Last updated: 2006 August 13
 ########################################################################
 
+__scriptname__ = 'XBMC Changelog'
+__author__ = 'Nuka1195/EnderW/killarny'
+__url__ = 'http://code.google.com/p/xbmc-scripting/'
+__credits__ = 'XBMC TEAM, freenode/#xbmc-scripting'
+__version__ = '1.0.1'
+
 import xbmcgui, xbmc
 dialog = xbmcgui.DialogProgress()
-dialog.create('XBMC Changelog')
+dialog.create(__scriptname__)
 dialog.update(5, 'Importing modules & initializing...')
+
 import urllib
-dialog.update(10)
-import datetime
-dialog.update(15)
-import sys
 dialog.update(20)
-from os import path
-dialog.update(25)
+import sys
+dialog.update(40)
+import os
+dialog.update(60)
 
 # url for the raw changelog
 CHANGELOG = 'http://appliedcuriosity.cc/xbox/changelog.txt'
 # how many lines of changes to show
 
-sys.path.append(sys.path[0] +  '\\extras\\lib')
-SkinPath = sys.path[0] +  '\\extras\\skins\\'
-
+sys.path.append(os.path.join( os.getcwd().replace( ";", "" ), 'resources', 'lib' ) )
+    
 def get_changes():
     # get the actual changelog.txt from sourceforge
     data = urllib.urlopen( CHANGELOG )
@@ -51,13 +55,8 @@ def get_changes():
 class windowOverlay( xbmcgui.Window ):
     def __init__( self ):
         global dialog
-        import  guibuilder
-        skin = xbmc.getSkinDir()
-        if (not path.exists(SkinPath + skin + '\\skin.xml')): skin = 'Default'
-        guibuilder.GUIBuilder(self, skinXML=SkinPath + skin + '\\skin.xml', imagePath=SkinPath + skin + '\\gfx',
-            title='XBMC Changelog', useDescAsKey=True, dlg=dialog, pct=50 )
+        self.setupGUI()
         if (not self.SUCCEEDED): 
-            #dialog.close()
             self.close()
         else: 
             dialog.close()
@@ -69,6 +68,20 @@ class windowOverlay( xbmcgui.Window ):
             finally:
                 dialog.close()
             self.controls[ 'textarea' ]['control'].setText( change_text )
+            
+    def setupGUI( self ):
+        global dialog
+        import guibuilder
+        cwd = os.getcwd().replace( ";", "" )
+        current_skin = xbmc.getSkinDir()
+        if ( not os.path.exists( os.path.join( cwd, 'resources', 'skins', current_skin ))): current_skin = 'default'
+        skin_path = os.path.join( cwd, 'resources', 'skins', current_skin )
+        image_path = os.path.join( skin_path, 'gfx' )
+        if ( self.getResolution() == 0 or self.getResolution() % 2 ): xml_file = 'skin_16x9.xml'
+        else: xml_file = 'skin.xml'
+        if ( not os.path.isfile( os.path.join( skin_path, xml_file ))): xml_file = 'skin.xml'
+        guibuilder.GUIBuilder( self, os.path.join( skin_path, xml_file ), image_path, 
+            title=__scriptname__, useDescAsKey=True, dlg=dialog, pct=60 )
 
     def onAction( self, action ):
         if action == 10: self.close()
