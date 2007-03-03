0ec76fa64a62bab038d68a0c4cc00befa466e18d
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-03-03 21:04:33 +0000

    changed: [XBMC Lyrics] Renumbered language strings for ALL languages(hope i didn't screw anybody up) added: [XBMC Lyrics] English language strings (254 & 255) added: [XBMC Lyrics] Ability to restart script after changes that require it. changed: [XBMC Lyrics] New language module changed: [XBMC Lyrics] Reworked settings, so now all scripts are easier to maintain. changed: [XBMC Lyrics] All skins to match settings changes(including adding new controls). changed: [XBMC Lyrics] Reworked update module. changed: [XBMC Lyrics] Default settings.xml changed: [XBMC Lyrics] lyricsutil.py -> utilities.py deleted: [XBMC Lyrics] Temporary folder

--- trunk/XBMC Lyrics/resources/lib/settings.py
+++ trunk/XBMC Lyrics/resources/lib/settings.py
@@ -1,43 +1,122 @@
-import os
+import sys, os
 import xbmc, xbmcgui
 import guibuilder
-import lyricsutil
-import traceback
+import utilities
 
 class GUI( xbmcgui.WindowDialog ):
+    """ Settings module: used for changing settings """
     def __init__( self, *args, **kwargs ):
-        try:
-            self._ = kwargs[ 'language' ]
-            self.__scriptname__ = kwargs[ 'scriptname' ]
-            self.__version__ = kwargs[ 'version' ]
-            self.setupGUI()
-            if ( not self.SUCCEEDED ): self.close()
-            else:
-                #self.show()
-                self._set_variables()
-                self._get_settings()
-                self._setup_scrapers()
-                self._set_controls_values()
-        except:
-            traceback.print_exc()
-            self.close()
-
+        self._ = kwargs[ "language" ]
+        self.__scriptname__ = sys.modules[ "__main__" ].__scriptname__
+        self.__version__ = sys.modules[ "__main__" ].__version__
+        self.setupGUI()
+        if ( not self.SUCCEEDED ): self._close_dialog()
+        else:
+            #self.show()
+            self._set_variables()
+            self._get_settings()
+            self._setup_special()
+            self._set_controls_values()
+            self._set_page_visible()
+            self._set_restart_required()
+            
     def setupGUI( self ):
-        cwd = os.getcwd().replace( ";", "" )
+        """ sets up the gui using guibuilder """
+        cwd = os.path.join( os.getcwd().replace( ";", "" ), "resources", "skins" )
         current_skin = xbmc.getSkinDir()
-        if ( not os.path.exists( os.path.join( cwd, 'resources', 'skins', current_skin ))): current_skin = 'default'
-        skin_path = os.path.join( cwd, 'resources', 'skins', current_skin )
-        image_path = os.path.join( skin_path, 'gfx' )
-        if ( self.getResolution() == 0 or self.getResolution() % 2 ): xml_file = 'settings_16x9.xml'
-        else: xml_file = 'settings.xml'
-        if ( not os.path.isfile( os.path.join( skin_path, xml_file ))): xml_file = 'settings.xml'
+        if ( not os.path.exists( os.path.join( cwd, current_skin ))): current_skin = "default"
+        skin_path = os.path.join( cwd, current_skin )
+        image_path = os.path.join( skin_path, "gfx" )
+        if ( self.getResolution() == 0 or self.getResolution() % 2 ): xml_file = "settings_16x9.xml"
+        else: xml_file = "settings.xml"
+        if ( not os.path.isfile( os.path.join( skin_path, xml_file ))): xml_file = "settings.xml"
         guibuilder.GUIBuilder( self, os.path.join( skin_path, xml_file ), image_path, useDescAsKey=True, language=self._, fastMethod=True, debug=False )
 
     def _set_variables( self ):
-        self.controller_action = lyricsutil.setControllerAction()
-        self.controls[ 'Version Label' ][ 'control' ].setLabel( '%s: %s' % ( self._( 100 ), self.__version__, ) )
-        
+        """ initializes variables """
+        self.current_page = 1
+        self.total_pages = 1
+        self.controller_action = utilities.setControllerAction()
+        self.get_control( "Title Label" ).setLabel( self.__scriptname__ )
+        self.get_control( "Version Label" ).setLabel( "%s: %s" % ( self._( 1006 ), self.__version__, ) )
+        self.get_control( "Update Button" ).setEnabled( xbmc.getCondVisibility( "System.InternetState" ) )
+        self.get_control( "Page Button" ).setVisible( self.total_pages > 1 )
+        self.get_control( "Page Button" ).setEnabled( self.total_pages > 1 )
+        # setEnabled( False ) if not used
+        self.get_control( "Credits Button" ).setVisible( False )
+        self.get_control( "Credits Button" ).setEnabled( False )
+
+    def _get_settings( self ):
+        """ reads settings """
+        self.settings = utilities.Settings().get_settings()
+
+    def _save_settings( self ):
+        """ saves settings """
+        ok = utilities.Settings().save_settings( self.settings )
+        if ( not ok ):
+            ok = xbmcgui.Dialog().ok( self.__scriptname__, self._( 230 ) )
+        else:
+            self._changed_message()
+
+    def _set_page_visible( self ):
+        """ shows the current page of settings """
+        xbmcgui.lock()
+        try: 
+            for control in range( 1, 19 ):
+                self.get_control( "Setting%s Button" % ( control, ) ).setVisible( self.current_page == int( ( control / 10 ) + 1 ) )
+                self.get_control( "Setting%s Button" % ( control, ) ).setEnabled( self.current_page == int( ( control / 10 ) + 1 ) )
+                self.get_control( "Setting%s Value" % ( control, ) ).setVisible( self.current_page == int( ( control / 10 ) + 1 ) )
+        except: pass
+        self.get_control( "Page Button" ).setLabel( "%s: %d/%d" % ( self._( 254 ), self.current_page, self.total_pages, ) )
+        xbmcgui.unlock()
+    
+    def _get_keyboard( self, default="", heading="" ):
+        """ shows a keyboard and returns a value """
+        keyboard = xbmc.Keyboard( default, heading )
+        keyboard.doModal()
+        if ( keyboard.isConfirmed() ):
+            return keyboard.getText()
+        return default
+
+    def _get_numeric( self, default="", heading="", type=3 ):
+        """ shows a numeric dialog and returns a value
+           - 0 : ShowAndGetNumber
+           - 1 : ShowAndGetDate
+           - 2 : ShowAndGetTime
+           - 3 : ShowAndGetIPAddress
+        """
+        dialog = xbmcgui.Dialog()
+        value = dialog.numeric( type, heading )
+        if ( value ): return value
+        else: return default
+
+    def _get_browse_dialog( self, default="", heading="", type=1, shares="files" ):
+        """ shows a browse dialog and returns a value
+            - 0 : ShowAndGetDirectory
+            - 1 : ShowAndGetFile
+            - 2 : ShowAndGetImage
+            - 3 : ShowAndGetWriteableDirectory
+        """
+        dialog = xbmcgui.Dialog()
+        value = dialog.browse( type, heading, shares )
+        if ( value ): return value
+        else: return default
+
+##### Start of unique defs #####################################################
+
+##### Special defs, script dependent, remember to call them from _setup_special #################
+    
+    def _set_restart_required( self ):
+        """ copies self.settings and add any settings that require a restart on change """
+        self.settings_original = self.settings.copy()
+        self.settings_restart = ( "scraper", )
+
+    def _setup_special( self ):
+        """ calls any special defs """
+        self._setup_scrapers()
+
     def _setup_scrapers( self ):
+        """ special def for setting up scraper choices """
         scrapers = []
         self.current_scraper = 0
         os.path.walk( os.path.join( os.getcwd().replace( ";", "" ), 'resources', 'scrapers' ), self._add_scraper, scrapers )
@@ -48,92 +127,124 @@ class GUI( xbmcgui.WindowDialog ):
                 break
         
     def _add_scraper( self, scrapers, path, files ):
-        tmp_scraper = os.path.split( path )[ 1 ]
-        if ( tmp_scraper.lower() != 'scrapers' ):
-            scrapers += [ tmp_scraper ]
+        """ special def for adding scrapers to a list variable """
+        folder = os.path.split( path )[ 1 ]
+        if ( folder.lower() != 'scrapers' ):
+            scrapers += [ folder ]
 
-    def _get_settings( self ):
-        self.settings = lyricsutil.Settings().get_settings()
-        self.SCRAPER = self.settings[ "scraper" ]
-        #self.settings_start = lyricsutil.Settings()
-        
+###### End of Special defs #####################################################
+
     def _set_controls_values( self ):
+        """ sets the value labels """
         #xbmcgui.lock()
-        self.controls['Scraper Button Value']['control'].setLabel( '%s' % ( self.settings[ "scraper" ], ) )
-        self.controls['Save Lyrics Button Value']['control'].setLabel( '%s' % ( str( self.settings[ "save_lyrics" ], ) ) )
-        self.controls['Save Folder Button']['control'].setEnabled( self.settings[ "save_lyrics" ] )
-        self.controls['Save Folder Button Value']['control'].setLabel( '%s' % ( self.settings[ "lyrics_path" ], ) )
-        self.controls['Save Folder Button Value']['control'].setEnabled( self.settings[ "save_lyrics" ]  )
-        self.controls['Smooth Scroll Button Value']['control'].setLabel( '%s' % ( str( self.settings[ "smooth_scrolling" ], ) ) )
-        self.controls['Show Viz Button Value']['control'].setLabel( '%s' % ( str( self.settings[ "show_viz" ], ) ) )
-        #xbmcgui.lock()
+        #self.controls['Scraper Button Value']['control'].setLabel( '%s' % ( self.settings[ "scraper" ], ) )
+        #self.controls['Save Lyrics Button Value']['control'].setLabel( '%s' % ( str( self.settings[ "save_lyrics" ], ) ) )
+        #self.controls['Save Folder Button']['control'].setEnabled( self.settings[ "save_lyrics" ] )
+        #self.controls['Save Folder Button Value']['control'].setLabel( '%s' % ( self.settings[ "lyrics_path" ], ) )
+        #self.controls['Save Folder Button Value']['control'].setEnabled( self.settings[ "save_lyrics" ]  )
+        #self.controls['Smooth Scroll Button Value']['control'].setLabel( '%s' % ( str( self.settings[ "smooth_scrolling" ], ) ) )
+        #self.controls['Show Viz Button Value']['control'].setLabel( '%s' % ( str( self.settings[ "show_viz" ], ) ) )
+
+        self.get_control( "Setting1 Value" ).setLabel( "%s" % ( self.settings[ "scraper" ], ) )
+        self.get_control( "Setting2 Value" ).setLabel( "%s" % ( str( self.settings[ "save_lyrics" ] ), ) )
+        self.get_control( "Setting3 Button" ).setEnabled( self.settings[ "save_lyrics" ] )
+        self.get_control( "Setting3 Value" ).setLabel( "%s" % ( self.settings[ "lyrics_path" ], ) )
+        self.get_control( "Setting3 Value" ).setEnabled( self.settings[ "save_lyrics" ] )
+        self.get_control( "Setting4 Value" ).setLabel( "%s" % ( str( self.settings[ "smooth_scrolling" ] ), ) )
+        self.get_control( "Setting5 Value" ).setLabel( "%s" % ( str( self.settings[ "show_viz" ] ), ) )
+        #xbmcgui.unlock()
     
-    def _toggle_scraper( self ):
+    def _change_setting1( self ):
+        """ changes settings #1 """
         self.current_scraper += 1
         if ( self.current_scraper == len( self.scrapers ) ): self.current_scraper = 0
         self.settings[ "scraper" ] = self.scrapers[ self.current_scraper ]
     
-    def _toggle_save_lyrics( self ):
+    def _change_setting2( self ):
+        """ changes settings #2 """
         self.settings[ "save_lyrics" ] = not self.settings[ "save_lyrics" ]
+        
+    def _change_setting3( self ):
+        """ changes settings #3 """
+        self.settings[ "lyrics_path" ] = self._get_browse_dialog( self.settings[ "lyrics_path" ], self._( 203 ) )
 
-    def _toggle_smooth_scrolling( self ):
+    def _change_setting4( self ):
+        """ changes settings #4 """
         self.settings[ "smooth_scrolling" ] = not self.settings[ "smooth_scrolling" ]
-
-    def _toggle_show_viz( self ):
+        
+    def _change_setting5( self ):
+        """ changes settings #5 """
         self.settings[ "show_viz" ] = not self.settings[ "show_viz" ]
-
-    def _browse_for_folder( self ):
-        dialog = xbmcgui.Dialog()
-        folder = dialog.browse( 3, self._( 40 ), 'files' )
-        if ( folder ):
-            self.settings[ "lyrics_path" ] = folder
     
-    def _save_settings( self ):
-        ret = lyricsutil.Settings().save_settings( self.settings )
-        if ( not ret ):
-            ok = xbmcgui.Dialog().ok( self._( 0 ), self._( 12 ) )
-        else:
-            if ( self.SCRAPER != self.settings[ "scraper" ]):
-                ok = xbmcgui.Dialog().ok( self._( 0 ), self._( 13 )  )
-            self._close_dialog()
-            
+##### End of unique defs ######################################################
+    
     def _update_script( self ):
+        """ checks for updates to the script """
         import update
-        updt = update.Update( language=self._, script=self.__scriptname__, version=self.__version__ )
+        updt = update.Update( language=self._ )
         del updt
+            
+    def _show_credits( self ):
+        """ shows a credit window """
+        import credits
+        c = credits.GUI( language=self._ )
+        c.doModal()
+        del c
 
-    def _close_dialog( self ):
+    def _change_page( self ):
+        """ changes pages """
+        self.current_page += 1
+        if ( self.current_page > self.total_pages ):
+            self.current_page = 1
+        self._set_page_visible()
+    
+    def _changed_message( self ):
+        """ checks for any changes that require a restart to take effect """
+        restart = False
+        for setting in self.settings_restart:
+            if ( self.settings_original[ setting ] != self.settings[ setting ] ):
+                restart = True
+                break
+        self._close_dialog( True, restart )
+
+    def _close_dialog( self, changed=False, restart=False ):
+        """ closes this dialog window """
+        self.changed = changed
+        self.restart = restart
         self.close()
         
+    def get_control( self, key ):
+        """ returns the control that matches the key """
+        try: return self.controls[ key ][ "control" ]
+        except: return None
+
     def onControl( self, control ):
-        if ( control is self.controls['Cancel Button']['control'] ):
-            self._close_dialog()
-        elif ( control is self.controls['Ok Button']['control'] ):
+        if ( control is self.get_control( "Ok Button" ) ):
             self._save_settings()
-        elif ( control is self.controls['Update Button']['control'] ):
+        elif ( control is self.get_control( "Cancel Button" ) ):
+            self._close_dialog()
+        elif ( control is self.get_control( "Update Button" ) ):
             self._update_script()
-        #elif ( control is self.controls['Credits Button']['control'] ):
-        #    self.showCredits()
+        elif ( control is self.get_control( "Credits Button" ) ):
+            self._show_credits()
+        elif ( control is self.get_control( "Page Button" ) ):
+            self._change_page()
         else:
-            if ( control is self.controls['Scraper Button']['control'] ):
-                self._toggle_scraper()
-            elif ( control is self.controls['Save Lyrics Button']['control'] ):
-                self._toggle_save_lyrics()
-            elif ( control is self.controls['Save Folder Button']['control'] ):
-                self._browse_for_folder()
-            elif ( control is self.controls['Smooth Scroll Button']['control'] ):
-                self._toggle_smooth_scrolling()
-            elif ( control is self.controls['Show Viz Button']['control'] ):
-                self._toggle_show_viz()
+            if ( control is self.get_control( "Setting1 Button" ) ):
+                self._change_setting1()
+            elif ( control is self.get_control( "Setting2 Button" ) ):
+                self._change_setting2()
+            elif ( control is self.get_control( "Setting3 Button" ) ):
+                self._change_setting3()
+            elif ( control is self.get_control( "Setting4 Button" ) ):
+                self._change_setting4()
+            elif ( control is self.get_control( "Setting5 Button" ) ):
+                self._change_setting5()
             self._set_controls_values()
             
     def onAction( self, action ):
-        try:
-            control = self.getFocus()
-            button_key = self.controller_action.get( action.getButtonCode(), 'n/a' )
-            if ( button_key == 'Keyboard Backspace Button' or button_key == 'B Button' or button_key == 'Remote Back Button' ):
-                self._close_dialog()
-            elif ( button_key == 'Keyboard ESC Button' or button_key == 'Back Button' or button_key == 'Remote Menu Button' ):
-                self._save_settings()
-        except:
-            traceback.print_exc()
+        button_key = self.controller_action.get( action.getButtonCode(), "n/a" )
+        if ( button_key == "Keyboard Backspace Button" or button_key == "B Button" or button_key == "Remote Back Button" ):
+            self._close_dialog()
+        elif ( button_key == "Keyboard ESC Button" or button_key == "Back Button" or button_key == "Remote Menu Button" ):
+            self._save_settings()
