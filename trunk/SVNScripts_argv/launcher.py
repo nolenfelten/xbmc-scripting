a73959f9748cc8baa6fb8c3adfe6c4c09e6dca7c
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-03-03 20:17:35 +0000

    changed: [SVNScripts_argv] put a dummy check and sleep in for System.InternetState, per GeminiServer's instructions.

--- trunk/SVNScripts_argv/launcher.py
+++ trunk/SVNScripts_argv/launcher.py
@@ -3,6 +3,7 @@ Launcher script
 """
 import sys, os
 import xbmc, xbmcgui
+dummy = xbmc.getCondVisibility( "System.InternetState" ) # per GeminiServers instructions
 
 if ( __name__ == "__main__" ):
     if ( len( sys.argv ) > 1 ):
@@ -10,14 +11,15 @@ if ( __name__ == "__main__" ):
         if ( os.path.isfile( "q:\\scripts\\%s\\default.py" % ( SCRIPT, ) ) ):
             xbmc.executebuiltin( 'XBMC.RunScript(q:\\scripts\\%s\\default.py)' % ( SCRIPT, ) )
         else:
-            if ( xbmc.getCondVisibility( 'system.internetstate' ) ):
+            xbmc.sleep( 300 ) # per GeminiServers instructions
+            if ( xbmc.getCondVisibility( "System.InternetState" ) ):
                 ok = xbmcgui.Dialog().yesno( "Warning! %s" % ( SCRIPT, ), "%s is not installed on your Xbox." % ( SCRIPT, ), "Would you like to download and install it?", "", 'skip', 'download' )
                 if ( ok ):
                     import svndownload
                     download = svndownload.Download( script=SCRIPT )
                     del download
                     if ( os.path.isfile( "q:\\scripts\\%s\\default.py" % ( SCRIPT, ) ) ):
-                        xbmc.executebuiltin( 'XBMC.RunScript(q:\\scripts\\%s\\default.py)' % ( SCRIPT, ) )
+                        xbmc.executebuiltin( "XBMC.RunScript(q:\\scripts\\%s\\default.py)" % ( SCRIPT, ) )
             else:
                 ok = xbmcgui.Dialog().ok( "Warning! %s" % ( SCRIPT, ), "%s is not installed on your Xbox." % ( SCRIPT, ), "You may download from SVN at:", "http://xbmc-scripting.googlecode.com/svn/trunk/%s" % ( SCRIPT.replace( " ", "%20" ), ) )
     else: ok = xbmcgui.Dialog().ok( "Invalid call to this script!", "You must supply a script name." )
