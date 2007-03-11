59fb84ecd3abd9dc73d81219c4d625cbea0113bb
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-03-11 01:10:14 +0000

    fixed: [XBMC Lyrics] saved filename uses unicode now. (Hopefully this is correct, it fixes Blue Oyster Cult) changed: [XBMC Lyrics] moved exceptions.txt file to T:\\script_data with lyrics default, in prep for adding the ability in GUI to add an exception.

--- XBMC Lyrics/resources/scrapers/lyricwiki/lyricsScraper.py
+++ XBMC Lyrics/resources/scrapers/lyricwiki/lyricsScraper.py
@@ -13,31 +13,31 @@ class _SongListParser( SGMLParser ):
     def reset( self ):
         SGMLParser.reset( self )
         self.song_list = []
-        self.url = 'None'
+        self.url = "None"
 
     def start_a( self, attrs ):
         for key, value in attrs:
-            if ( key == 'href' ):
-                if ( not value[ 0 ] == '#' and not value[ : 10 ] =='/Category:' and
-                    not value[ : 11 ] == '/LyricWiki:' and not value == '/Main_Page' and
-                    not value[ : 15 ] == '/Special:Search' ):
+            if ( key == "href" ):
+                if ( not value[ 0 ] == "#" and not value[ : 10 ] =="/Category:" and
+                    not value[ : 11 ] == "/LyricWiki:" and not value == "/Main_Page" and
+                    not value[ : 15 ] == "/Special:Search" ):
                     self.url = value
-            elif ( key == 'title' ):
-                if ( urllib.unquote( self.url[ 1 : ] ) == urllib.unquote( value.replace( ' ', '_' ).replace( '&amp;', '&' ) ) ):
-                    self.song_list += [ ( unicode( value[ value.find( ':' ) + 1 : ], 'utf-8', 'ignore' ), self.url, ) ]
+            elif ( key == "title" ):
+                if ( urllib.unquote( self.url[ 1 : ] ) == urllib.unquote( value.replace( " ", "_" ).replace( "&amp;", "&" ) ) ):
+                    self.song_list += [ ( unicode( value[ value.find( ":" ) + 1 : ], "utf-8", "ignore" ), self.url, ) ]
             else:
-                self.url = 'None'
+                self.url = "None"
 
 class _LyricsParser( SGMLParser ):
     """ Parses an html document for song lyrics """
     def reset( self ):
         SGMLParser.reset( self )
         self.lyrics_found = False
-        self.lyrics = unicode( '', 'utf-8' )
+        self.lyrics = unicode( "", "utf-8" )
 
     def start_div( self, attrs ):
         for key, value in attrs:
-            if ( key == 'id' and value == 'lyric' ):
+            if ( key == "id" and value == "lyric" ):
                 self.lyrics_found = True
             else: self.lyrics_found = False
                 
@@ -47,7 +47,7 @@ class _LyricsParser( SGMLParser ):
     def handle_data( self, text ):
         if ( self.lyrics_found ):
             try:
-                self.lyrics += unicode( text, 'utf-8', 'ignore' )
+                self.lyrics += unicode( text, "utf-8", "ignore" )
             except:
                 # bad data so skip it
                 pass
@@ -55,12 +55,12 @@ class _LyricsParser( SGMLParser ):
 class LyricsFetcher:
     """ required: Fetcher class for www.lyricwiki.org """
     def __init__( self ):
-        self.url = 'http://www.lyricwiki.org'
+        self.url = "http://www.lyricwiki.org"
         self.exceptions = self._get_exceptions()
         
     def get_lyrics( self, artist, song ):
         """ *required: Returns song lyrics or a list of choices from artist & song """
-        url = self.url + '/%s:%s'
+        url = self.url + "/%s:%s"
         artist = self._format_param( artist )
         song = self._format_param( song )
         lyrics = self._fetch_lyrics( url % ( artist, song, ) )
@@ -79,16 +79,27 @@ class LyricsFetcher:
         """ Reads a rules file for exceptions in formatting artist or song """
         exceptions = []
         try:
-            if ( __name__ == '__main__' ):
-                ex_file = open( os.path.join( os.getcwd().replace( ";", "" ) , 'exceptions.txt' ), 'r' )
+            if ( __name__ == "__main__" ):
+                ex_path = os.path.join( os.getcwd().replace( ";", "" ), "exceptions.txt" )
             else:
-                ex_file = open( os.path.join( os.path.dirname( sys.modules[ 'lyricsScraper' ].__file__ ) , 'exceptions.txt' ), 'r' )
-            ex_list = ex_file.read().split('||')
+                ex_path = os.path.join( "T:\\script_data", sys.modules[ "__main__" ].__scriptname__, "scrapers", os.path.split( os.path.dirname( sys.modules[ "lyricsScraper" ].__file__ ) )[ 1 ], "exceptions.txt" )
+                if ( not os.path.isfile( ex_path ) ):
+                    self._make_exception_file( ex_path )
+            ex_file = open( ex_path, "r" )
+            ex_list = ex_file.read().split("||")
             ex_file.close()
             for ex in ex_list:
-                exceptions += [ ( ex.split( '|' )[0], ex.split( '|' )[1], ) ]
+                exceptions += [ ( ex.split( "|" )[0], ex.split( "|" )[1], ) ]
         except: pass
         return exceptions
+
+    def _make_exception_file( self, ex_path ):
+        try:
+            os.makedirs( os.path.split( ex_path )[ 0 ] )
+            ex_file = open( ex_path, "w" )
+            ex_file.write( "Ac_dc|AC_DC||Abba|ABBA" )
+            ex_file.close()
+        except: pass
 
     def _fetch_lyrics( self, url ):
         """ Fetch lyrics if available """
@@ -96,12 +107,12 @@ class LyricsFetcher:
             # Open url or local file (if debug == True)
             if (not debug): usock = urllib.urlopen( url )
             else:
-                usock = open( os.path.join( os.getcwd().replace( ";", "" ), 'lyrics_source.txt' ), 'r' )
+                usock = open( os.path.join( os.getcwd().replace( ";", "" ), "lyrics_source.txt" ), "r" )
             htmlSource = usock.read()
             usock.close()
             # Save htmlSource to a file for testing scraper (if debugWrite == True)
             if (debugWrite):
-                usock = open( os.path.join( os.getcwd().replace( ";", "" ), 'lyrics_source.txt' ), 'w' )
+                usock = open( os.path.join( os.getcwd().replace( ";", "" ), "lyrics_source.txt" ), "w" )
                 usock.write( htmlSource )
                 usock.close
             # Parse htmlSource for lyrics
@@ -115,16 +126,16 @@ class LyricsFetcher:
     def _get_song_list( self, artist ):
         """ If no lyrics found, fetch a list of choices """
         try:
-            url = self.url + '/%s'
+            url = self.url + "/%s"
             # Open url or local file (if debug == True)
             if (not debug): usock = urllib.urlopen( url % ( artist, ) )
             else:
-                usock = open( os.path.join( os.getcwd().replace( ";", "" ), 'songs_source.txt' ), 'r' )
+                usock = open( os.path.join( os.getcwd().replace( ";", "" ), "songs_source.txt" ), "r" )
             htmlSource = usock.read()
             usock.close()
             # Save htmlSource to a file for testing scraper (if debugWrite == True)
             if ( debugWrite ):
-                usock = open( os.path.join( os.getcwd().replace( ";", "" ), 'songs_source.txt' ), 'w' )
+                usock = open( os.path.join( os.getcwd().replace( ";", "" ), "songs_source.txt" ), "w" )
                 usock.write( htmlSource )
                 usock.close
             # Parse htmlSource for song links
@@ -150,13 +161,13 @@ class LyricsFetcher:
     def _format_param( self, param ):
         """ Converts param to the form expected by www.lyricwiki.org """
         caps = True
-        result = ''
+        result = ""
         # enumerate thru string to properly capitalize words (why doesn't title() do this properly?)
         for letter in param.upper().strip():
-            if ( letter == ' ' ):
-                letter = '_'
+            if ( letter == " " ):
+                letter = "_"
                 caps = True
-            elif ( letter in '(_-.' ):
+            elif ( letter in "(_-." ):
                 caps = True
             elif ( unicode( letter.isalpha() ) and caps ):
                 caps = False
@@ -164,7 +175,7 @@ class LyricsFetcher:
                 letter = letter.lower()
                 caps = False
             result += letter
-        result = result.replace( '/', '_' )
+        result = result.replace( "/", "_" )
         # replace any exceptions
         for ex in self.exceptions:
             result = result.replace( ex[ 0 ], ex[ 1 ] )
@@ -175,18 +186,18 @@ class LyricsFetcher:
     def _clean_text( self, text ):
         """ Convert line terminators and html entities """
         try:
-            text = text.replace( '\t', '' )
-            text = text.replace( '<br> ', '\n' )
-            text = text.replace( '<br>', '\n' )
-            text = text.replace( '<br /> ', '\n' )
-            text = text.replace( '<br />', '\n' )
-            text = text.replace( '<div>', '\n' )
-            text = text.replace( '> ', '\n' )
-            text = text.replace( '>', '\n' )
-            text = text.replace( '&amp;', '&' )
-            text = text.replace( '&gt;', '>' )
-            text = text.replace( '&lt;', '<' )
-            text = text.replace( '&quot;', '"' )
+            text = text.replace( "\t", "" )
+            text = text.replace( "<br> ", "\n" )
+            text = text.replace( "<br>", "\n" )
+            text = text.replace( "<br /> ", "\n" )
+            text = text.replace( "<br />", "\n" )
+            text = text.replace( "<div>", "\n" )
+            text = text.replace( "> ", "\n" )
+            text = text.replace( ">", "\n" )
+            text = text.replace( "&amp;", "&" )
+            text = text.replace( "&gt;", ">" )
+            text = text.replace( "&lt;", "<" )
+            text = text.replace( "&quot;", '"' )
         except: pass
         return text
 
@@ -194,16 +205,16 @@ class LyricsFetcher:
 debug = False
 debugWrite = False
 
-if ( __name__ == '__main__' ):
+if ( __name__ == "__main__" ):
     # used to test get_lyrics() 
-    artist = [ "ABBA", "Jem","Stealers Wheel","Paul McCartney & Wings","ABBA","AC/DC", "Tom Jones", "Kim Mitchell", "Ted Nugent", "Blue Öyster Cult", "The 5th Dimension", "Big & Rich", "Don Felder" ]
-    song = [ "S.O.S","24","Stuck in the middle with you","Band on the run", "Dancing Queen", "T.N.T.", "She's A Lady", "Go for Soda", "Free-for-all", "(Don't Fear) The Reaper", "Age of Aquarius", "Save a Horse (Ride a Cowboy)", "Heavy Metal (Takin' a Ride)" ]
-    for cnt in range( 3, 4 ):
+    artist = [ "The Charlie Daniels Band", "ABBA", "Jem","Stealers Wheel","Paul McCartney & Wings","ABBA","AC/DC", "Tom Jones", "Kim Mitchell", "Ted Nugent", "Blue Öyster Cult", "The 5th Dimension", "Big & Rich", "Don Felder" ]
+    song = [ "(What This World Needs Is) A Few More Rednecks", "S.O.S","24","Stuck in the middle with you","Band on the run", "Dancing Queen", "T.N.T.", "She's A Lady", "Go for Soda", "Free-for-all", "(Don't Fear) The Reaper", "Age of Aquarius", "Save a Horse (Ride a Cowboy)", "Heavy Metal (Takin' a Ride)" ]
+    for cnt in range( 0, 1 ):
         lyrics = LyricsFetcher().get_lyrics( artist[ cnt ], song[ cnt ] )
     
     # used to test get_lyrics_from_list() 
-    #url = ('Big & Rich:Save a Horse (Ride a Cowboy)', '/Big_%26_Rich:Save_a_Horse_%28Ride_a_Cowboy%29')
-    #url = (u'Stuck In The Middle With You', '/Stealers_Wheel:Stuck_In_The_Middle_With_You')
+    #url = ("Big & Rich:Save a Horse (Ride a Cowboy)", "/Big_%26_Rich:Save_a_Horse_%28Ride_a_Cowboy%29")
+    #url = (u"Stuck In The Middle With You", "/Stealers_Wheel:Stuck_In_The_Middle_With_You")
     #lyrics = LyricsFetcher().get_lyrics_from_list( url )
     
     # print the results
@@ -211,4 +222,4 @@ if ( __name__ == '__main__' ):
         for song in lyrics:
             print song
     else:
-        print lyrics.encode( 'utf-8', 'ignore' )
+        print lyrics.encode( "utf-8", "ignore" )
