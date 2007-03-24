3dde7c21f2407aeff92a7e1d25a41088d79f1165
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-03-24 21:46:07 +0000

    changed: [T3CH Downloader] changed url to ip changed: [T3CH Downloader] cleaned up code

--- trunk/T3CH Downloader/resources/lib/language.py
+++ trunk/T3CH Downloader/resources/lib/language.py
@@ -1,52 +1,65 @@
-import sys, os
+"""
+Language module
+
+Nuka1195
+"""
+
+import os
 import xbmc
 import xml.dom.minidom
 
+
 class Language:
-    """ Language Class: Returns a localized string """
+    """ Language Class: creates a dictionary of localized strings { int: string } """
     def __init__( self ):
-        self._get_language()
+        """ initializer """
+        # language folder
+        base_path = os.path.join( os.getcwd().replace( ";", "" ), "resources", "language" )
+        # get the current language
+        language = self._get_language( base_path )
+        # create strings dictionary
+        self._create_localized_dict( base_path, language )
         
-    def _get_language( self ):
-        """ gets the current language """
-        self.strings = {}
-        module_dir = os.path.dirname( sys.modules['language'].__file__ )
-        cwd = os.path.join( os.path.dirname( module_dir ), 'language' )
+    def _get_language( self, base_path ):
+        """ returns the current language if a strings.xml file exists else returns english """
+        # get the current users language setting
         language = xbmc.getLanguage().lower()
-        language_path = os.path.join( cwd, language, 'strings.xml' )
-        if ( not os.path.isfile( language_path ) ):
-            language = 'english'
-            language_path = os.path.join( cwd, language, 'strings.xml' )
-        success = self._parse_strings_file( language_path )
-        if ( language != 'english' ):
-            success = self._parse_strings_file( os.path.join( cwd, 'english', 'strings.xml' ) )
+        # if no strings.xml file exists, default to english
+        if ( not os.path.isfile( os.path.join( base_path, language, "strings.xml" ) ) ):
+            language = "english"
+        return language
+
+    def _create_localized_dict( self, base_path, language ):
+        """ initializes self.strings and calls _parse_strings_file """
+        # localized strings dictionary
+        self.strings = {}
+        # add localized strings
+        self._parse_strings_file( os.path.join( base_path, language, "strings.xml" ) )
+        # fill-in missing strings with english strings
+        if ( language != "english" ):
+            self._parse_strings_file( os.path.join( base_path, "english", "strings.xml" ) )
         
     def _parse_strings_file( self, language_path ):
-        """ Main parser for the strings.xml file """
+        """ adds localized strings to self.strings dictionary """
         try:
             # load and parse strings.xml file
             doc = xml.dom.minidom.parse( language_path )
-
             # make sure this is a valid <strings> xml file
             root = doc.documentElement
-            if ( not root or root.tagName != 'strings' ): raise
-            
-            # parse and resolve each <string>
-            strings = root.getElementsByTagName( 'string' )
+            if ( not root or root.tagName != "strings" ): raise
+            # parse and resolve each <string id="#"> tag
+            strings = root.getElementsByTagName( "string" )
             for string in strings:
-                if ( string.hasAttributes() ):
-                    string_id = string.getAttribute( 'id' )
-                if ( string_id and not self.strings.has_key( string_id ) ):
+                string_id = string.getAttribute( "id" )
+                # if a valid id add it to self.strings dictionary
+                if ( string_id and string_id not in self.strings ):
                     if ( string.hasChildNodes() ): 
                         self.strings[ int( string_id ) ] = string.firstChild.nodeValue
-            success = True
         except:
-            print "ERROR: Language file %s can't be opened" % ( language_path, )
-            success = False
+            print "ERROR: Language file %s can't be parsed" % ( language_path, )
         try: doc.unlink()
         except: pass
-        return success
 
-    def string( self, code ):
-        """ Returns the localized string if it exists """
+    def localized( self, code ):
+        """ returns the localized string if it exists """
         return self.strings.get( int( code ), str( code ) )
