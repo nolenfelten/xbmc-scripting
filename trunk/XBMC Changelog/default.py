6ac2c8d22109dba3c93725a1b1b1514d6283cfc0
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-04-05 13:19:03 +0000

    changed: [XBMC Changelog] _fetch_changelog() sets class variables instead of returning them

--- trunk/XBMC Changelog/default.py
+++ trunk/XBMC Changelog/default.py
@@ -20,26 +20,25 @@ resource_path = os.path.join( os.getcwd().replace( ";", "" ), "resources" )
 class GUI( xbmcgui.WindowXML ):
     def __init__( self, *args, **kwargs ):
         base_url = "http://appliedcuriosity.cc/xbox/changelog.txt"
-        self.header, self.changelog, self.last_change = self.fetch_changelog( base_url )
         self.exit_script = [ 247, 275, 61467 ]
+        self._fetch_changelog( base_url )
 
     def onInit( self ):
         self.getControl( 4 ).setLabel( self.header )
         self.getControl( 5 ).setText( self.changelog )
         self.getControl( 6 ).setLabel( "Last change: %s" % self.last_change )
 
-    def fetch_changelog( self, base_url ):
+    def _fetch_changelog( self, base_url ):
         dialog = xbmcgui.DialogProgress()
         dialog.create( "Fetching changelog..." )
         import urllib
         usock = urllib.urlopen( base_url )
         data = usock.readlines()
         usock.close()
-        header = "".join( data[ 6 : 8 ] )
-        changelog = "".join( data[ 8 : ] )
-        last_change = data[ 8 ].split()[0]
+        self.header = "".join( data[ 6 : 8 ] )
+        self.changelog = "".join( data[ 8 : ] )
+        self.last_change = data[ 8 ].split()[0]
         dialog.close()
-        return header, changelog, last_change
 
     def onClick( self, controlId ):
         pass
