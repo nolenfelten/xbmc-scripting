a546496137c9e7fe6cdca00615c0dfdd73cc9741
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-04-06 05:03:47 +0000

    changed: [Apple Movie Trailers] code organization changed: [Apple Movie Trailers] xTV thumbnail.tbn

--- Apple Movie Trailers/resources/lib/chooser.py
+++ Apple Movie Trailers/resources/lib/chooser.py
@@ -5,8 +5,8 @@ Nuka1195
 """
 
 import os
-import xbmc
 import xbmcgui
+#import xbmc
 import guibuilder
 import utilities
 
@@ -16,47 +16,37 @@ class GUI( xbmcgui.WindowDialog ):
         self.base_path = os.path.join( utilities.BASE_RESOURCE_PATH, "skins" )
         self.gui_loaded = self._load_gui( skin, language )
         if ( not self.gui_loaded ): self._close_dialog()
-        else:
-            self._set_variables()
             
     def _load_gui( self, skin, language ):
         """ sets up the gui using guibuilder """
         gb = guibuilder.GUIBuilder()
         gui_loaded, image_path = gb.create_gui( self, skin=skin, xml_name="chooser", language=language )
         return gui_loaded
-
-    def _set_variables( self ):
-        self.controller_action = utilities.setControllerAction()
             
     def show_chooser( self, choices, selection, list_control, title ):
         self.controls[ "Chooser Label" ][ "control" ].setLabel( title )
-        self.choices = choices
-        self.selection = selection
         self.list_control = list_control
-        self._setup_list()
+        self._setup_list( choices, selection )
         if ( list_control == 1 ):
             self._get_thumb( self.controls[ "Chooser List1" ][ "control" ].getSelectedItem().getLabel() )
         self.doModal()
 
-    def _setup_list( self ):
+    def _setup_list( self, choices, selection ):
         xbmcgui.lock()
         self.controls[ "Chooser List1" ][ "control" ].setVisible( self.list_control == 1 )
         self.controls[ "Chooser List2" ][ "control" ].setVisible( self.list_control == 2 )
         self.controls[ "Chooser Warning Label" ][ "control" ].setVisible( False )
         self.controls[ "Chooser Thumb" ][ "control" ].setVisible( self.list_control == 1 )
         self.controls[ "Chooser List%d" % self.list_control ][ "control" ].reset()
-        for choice in self.choices:
+        for choice in choices:
             self.controls[ "Chooser List%d" % self.list_control ][ "control" ].addItem( choice )
-        self.controls[ "Chooser List%d" % self.list_control ][ "control" ].selectItem( self.selection )
+        self.controls[ "Chooser List%d" % self.list_control ][ "control" ].selectItem( selection )
         self.setFocus( self.controls[ "Chooser List%d" % self.list_control ][ "control" ] )
         xbmcgui.unlock()
 
     def _get_thumb( self, choice ):
         thumbnail = os.path.join( self.base_path, choice, "thumbnail.tbn" )
-        if ( os.path.isfile( thumbnail ) ):
-            self.controls[ "Chooser Thumb" ][ "control" ].setImage( thumbnail )
-        else:
-            self.controls[ "Chooser Thumb" ][ "control" ].setImage( "" )
+        self.controls[ "Chooser Thumb" ][ "control" ].setImage( thumbnail )
         self.controls[ "Chooser Warning Label" ][ "control" ].setVisible( os.path.isfile( os.path.join( self.base_path, choice, "warning.txt" ) ) )
             
     def _close_dialog( self, selection=None ):
@@ -65,10 +55,7 @@ class GUI( xbmcgui.WindowDialog ):
         
     def onControl( self, control ):
         #xbmc.sleep( 5 )
-        if ( control is self.controls[ "Chooser List1" ][ "control" ] ):
-            self._close_dialog( self.controls[ "Chooser List1" ][ "control" ].getSelectedPosition() )
-        elif ( control is self.controls[ "Chooser List2" ][ "control" ] ):
-            self._close_dialog( self.controls[ "Chooser List2" ][ "control" ].getSelectedPosition() )
+        self._close_dialog( control.getSelectedPosition() )
 
     def onAction( self, action ):
         if ( action.getButtonCode() in utilities.CANCEL_DIALOG ):
