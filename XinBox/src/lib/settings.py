1daf121da41f6ab83030d7a13100e2bcc9b91be0
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-04-15 12:17:42 +0000

    Added: New Images By X3lectric  - Big Thanks! more to come I hope :-p

--- XinBox/src/lib/settings.py
+++ XinBox/src/lib/settings.py
@@ -6,7 +6,63 @@ lang = language.Language().string
 class Settings( xbmcgui.WindowXML ):        
     def onInit(self):
         self.control_action = xib_util.setControllerAction()
+        self.getmycontrols()
+        self.getSettings()
+        self.sortsettings()
+        for item in self.listitems:
+            self.addItem(xbmcgui.ListItem(item))
+        xbmcgui.unlock()
 
+    def getmycontrols(self):
+        self.labelist = self.getControl(60)
+        for i in range (0,9):
+            self.labelist.addItem(lang(88+i))
+        
+    def sortsettings(self):
+        self.listitems = []
+        self.server1 = self.settings.server1
+        self.user1 = self.settings.user1
+        self.pass1 = self.settings.pass1
+        self.ssl1 = self.settings.ssl1
+        self.server2 = self.settings.server2
+        self.user2 = self.settings.user2
+        self.pass2 = self.settings.pass2
+        self.ssl2 = self.settings.ssl2
+        self.mpenable = self.settings.mpenable
+        self.mp = self.settings.mp
+        if self.server1 == "-":
+            self.listitems.append(lang(81))
+        else:
+            self.listitems.append(str(self.server1))
+        if self.user1 == "-":
+            self.listitems.append(lang(82))
+        else:
+            self.listitems.append(str(self.user1))
+        if self.pass1 == "-":
+            self.listitems.append(lang(83))
+        else:
+            self.listitems.append(str(self.pass1))
+        if self.ssl1 == "-":
+            self.listitems.append(lang(84))
+        else:
+            self.listitems.append(str(self.ssl1))
+        if self.server2 == "-":
+            self.listitems.append(lang(85))
+        else:
+            self.listitems.append(str(self.server2))
+        if self.user2 == "-":
+            self.listitems.append(lang(86))
+        else:
+            self.listitems.append(str(self.user2))
+        if self.pass2 == "-":
+            self.listitems.append(lang(87))
+        else:
+            self.listitems.append(str(self.pass2))
+        if self.ssl2 == "-":
+            self.listitems.append(lang(88))
+        else:
+            self.listitems.append(str(self.ssl2))        
+            
     def onClick(self, controlID):
         if ( controlID == 99):
             self.close()
@@ -19,3 +75,6 @@ class Settings( xbmcgui.WindowXML ):
 
     def onFocus(self, controlID):
         print 'The control with id="5" just got focus'   
+
+    def getSettings( self ):
+        self.settings = xib_util.Settings()
