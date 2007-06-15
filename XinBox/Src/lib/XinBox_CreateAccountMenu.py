db1cde14ae3a89e67f03c7121d5fa60af70496bf
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-06-15 10:35:59 +0000

    Updated: Create Account Menu

--- XinBox/Src/lib/XinBox_CreateAccountMenu.py
+++ XinBox/Src/lib/XinBox_CreateAccountMenu.py
@@ -24,37 +24,54 @@ class GUI( xbmcgui.WindowXML ):
     def onInit(self):
         self.setupcontrols()
         self.setupvars()
+        self.set_controls_values()
         self.default = False
-        xbmcgui.unlock()
+
 
     def setupcontrols(self):
-        ListItems = [xbmcgui.ListItem(_(51)),
-                     xbmcgui.ListItem(_(52)),
-                     xbmcgui.ListItem(_(53)),]
-        for item in ListItems:
-            self.addItem(item)
+        xbmcgui.lock()
+        for x in range( 1, 4 ):
+            self.addItem(xbmcgui.ListItem())
         self.getControl(80).setLabel(_(50))
+        self.buttonids = [61,62,63,64,65]
+        for ID in self.buttonids:
+            self.getControl(ID).setLabel(_(ID))
+        self.getControl(82).setLabel(_(20))
+        self.getControl(83).setLabel(_(21))
+        xbmcgui.unlock()
+
+    def set_controls_values(self): #thanks to Nuka for this!
+        xbmcgui.lock()
+        try:
+            self.getListItem(0).setLabel(_(51))
+            self.getListItem(0).setLabel2(self.testsetting)
+            self.getListItem(1).setLabel(_(52))
+            self.getListItem(1).setLabel2("")
+            self.getListItem(2).setLabel(_(53))
+        except: traceback.print_exc()
+        xbmcgui.unlock()
 
     def setupvars(self):
         self.control_action = xib_util.setControllerAction()
-        
+        self.testsetting = ""
+
+      
     def onFocus(self, controlID):
         pass
     
     def onClick(self, controlID):
         if ( controlID == 51):
             if self.getCurrentListPosition() == 0: 
-                pass
+                self.testsetting = "Stanley87"
+                self.set_controls_values()
             elif self.getCurrentListPosition() == 1:
                 pass
             elif self.getCurrentListPosition() == 2:
                 self.default = not self.default
                 self.getControl(104).setSelected(self.default)
-            elif self.getCurrentListPosition() == 3:
-                self.launchmenu("XinBox_LoginMenu")
-            elif self.getCurrentListPosition() == 4:
-                self.close()
-                    
+        elif ( controlID == 65 ):
+            self.close()
+
     def onAction( self, action ):
         button_key = self.control_action.get( action.getButtonCode(), 'n/a' )
         actionID   =  action.getId()
@@ -64,10 +81,10 @@ class GUI( xbmcgui.WindowXML ):
             self.close()
         elif ( button_key == 'Keyboard Menu Button' or button_key == 'Y Button' or button_key == 'Remote Title' ):
             if focusid == 51:
-                print "HERE"
-                try:
-                    self.launchinfo(105 + self.getCurrentListPosition(),self.getListItem(self.getCurrentListPosition()).getLabel())
-                except:traceback.print_exc()
+                self.launchinfo(105 + self.getCurrentListPosition(),self.getListItem(self.getCurrentListPosition()).getLabel())
+            else:
+                self.launchinfo(focusid+47,_(focusid))
+                
 
     def launchinfo(self, focusid, label):
         dialog = XinBox_InfoDialog.GUI("XinBox_InfoDialog.xml",scriptpath + "src","DefaultSkin")
