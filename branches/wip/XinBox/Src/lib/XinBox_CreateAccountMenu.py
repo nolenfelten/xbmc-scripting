393312eeb4d5565a2746adfecaa59e350ab6d6ac
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-06-16 03:05:13 +0000

    Added: Settings.py (cheers Donno)

--- branches/wip/XinBox/Src/lib/XinBox_CreateAccountMenu.py
+++ branches/wip/XinBox/Src/lib/XinBox_CreateAccountMenu.py
@@ -11,27 +11,33 @@
 ######################################
 import xbmc, sys, os, default,xib_util
 import xbmcgui, language, time
-import XinBox_InfoDialog
+import XinBox_InfoDialog, traceback
+from settings import Settings
 
 scriptpath = default.__scriptpath__
 
 _ = language.Language().string  
+defSettings = {_(51): ["Add Acount Name Here","text"],
+                _(52): ["Add Account Password Here","text"],
+                _(53): ["false","boolean"],}
 
 class GUI( xbmcgui.WindowXML ):
     def __init__(self,strXMLname, strFallbackPath,strDefaultName,bforeFallback=0):
         print "welcome"
 
     def onInit(self):
+        try:
+            self.scriptSettings = Settings("XinBox_Account_Settings.xml","XinBox Account Settings", defSettings)
+        except:
+            traceback.print_exc()
         self.setupcontrols()
         self.setupvars()
-        self.set_controls_values()
+        self.buildlist()
         self.default = False
 
 
     def setupcontrols(self):
         xbmcgui.lock()
-        for x in range( 1, 4 ):
-            self.addItem(xbmcgui.ListItem())
         self.getControl(80).setLabel(_(50))
         self.buttonids = [61,62,63,64,65]
         for ID in self.buttonids:
@@ -40,17 +46,22 @@ class GUI( xbmcgui.WindowXML ):
         self.getControl(83).setLabel(_(21))
         xbmcgui.unlock()
 
-    def set_controls_values(self): #thanks to Nuka for this!
+    def buildlist(self):
         xbmcgui.lock()
-        try:
-            self.getListItem(0).setLabel(_(51))
-            self.getListItem(0).setLabel2(self.testsetting)
-            self.getListItem(1).setLabel(_(52))
-            self.getListItem(1).setLabel2("")
-            self.getListItem(2).setLabel(_(53))
-        except: traceback.print_exc()
+        for set in self.scriptSettings.settings:
+            self.addItem(self.SettingListItem(set, self.scriptSettings.getSetting(set)))
         xbmcgui.unlock()
 
+    def SettingListItem(self, label1,label2):
+        if label2 == "true":
+            self.getControl(104).setSelected(True)
+            return xbmcgui.ListItem(label1,"","","")
+        elif label2 == "false":
+            self.getControl(104).setSelected(False)
+            return xbmcgui.ListItem(label1,"","","")
+        else:
+            return xbmcgui.ListItem(label1,label2,"","")
+    
     def setupvars(self):
         self.control_action = xib_util.setControllerAction()
         self.testsetting = ""
@@ -61,16 +72,34 @@ class GUI( xbmcgui.WindowXML ):
     
     def onClick(self, controlID):
         if ( controlID == 51):
-            if self.getCurrentListPosition() == 0: 
-                self.testsetting = "Stanley87"
-                self.set_controls_values()
-            elif self.getCurrentListPosition() == 1:
-                pass
-            elif self.getCurrentListPosition() == 2:
-                self.default = not self.default
-                self.getControl(104).setSelected(self.default)
+            curPos  = self.getCurrentListPosition()
+            curItem = self.getListItem(curPos)
+            curName = curItem.getLabel()
+            type = self.scriptSettings.getSettingType(curName)
+            if (type == "text"):
+                value = self.showKeyboard(_(66) % curName,curItem.getLabel2())
+                curItem.setLabel2(value)
+                self.scriptSettings.setSetting(curName,value)
+            elif (type == "boolean"):
+                if self.scriptSettings.getSetting(curName,"true") == "false":
+                    self.scriptSettings.setSetting(curName,"true")
+                    self.getControl(104).setSelected(True)
+                else:
+                    self.scriptSettings.setSetting(curName,"false")
+                    self.getControl(104).setSelected(False)
+        elif ( controlID == 64 ):
+            self.scriptSettings.saveXMLfromArray()
         elif ( controlID == 65 ):
             self.close()
+            
+    def showKeyboard(self, heading,default=""):
+        # Open the Virutal Keyboard.
+        keyboard = xbmc.Keyboard(default,heading)
+        keyboard.doModal()
+        if (keyboard.isConfirmed()):
+            return keyboard.getText()
+        else:
+            return default
 
     def onAction( self, action ):
         button_key = self.control_action.get( action.getButtonCode(), 'n/a' )
@@ -91,4 +120,3 @@ class GUI( xbmcgui.WindowXML ):
         dialog.setupvars(focusid, label)
         dialog.doModal()
         del dialog
-
