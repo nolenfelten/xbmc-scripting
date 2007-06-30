43db6f5c7d921895bf1977dba8489cc98eada271
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-06-30 12:33:14 +0000

    Added: Inbox Menu Updated: Email Engine

--- branches/wip/XinBox/Src/lib/XinBox_Inbox.py
+++ branches/wip/XinBox/Src/lib/XinBox_Inbox.py
@@ -1,16 +1,24 @@
 
 
 import xbmc,xbmcgui, time, sys, os,traceback
-import XinBox_Util
+import XinBox_Util, email, re
 from XinBox_Settings import Settings
 from XinBox_EmailEngine import Checkemail
+from os.path import join, exists
+from os import mkdir, remove
+SETTINGDIR = "P:\\script_data\\XinBox\\"
+ACCOUNTSDIR = "P:\\script_data\\XinBox\\Accounts\\"
+from sgmllib import SGMLParser    
 
 class GUI( xbmcgui.WindowXML ):
     def __init__(self,strXMLname, strFallbackPath,strDefaultName,lang=False,theinbox=False,account=False,title=False):
-        self.language = lang
-        self.inbox = theinbox
-        self.account = account
-        self.title = title
+        try:
+            self.language = lang
+            self.inbox = theinbox
+            self.account = account
+            self.title = title
+            self.ibfolder = join(ACCOUNTSDIR,str(hash(self.account)),str(hash(self.inbox))) + "\\"
+        except:traceback.print_exc()
 
     def loadsettings(self):
         self.settings = Settings("XinBox_Settings.xml",self.title,"")
@@ -23,6 +31,7 @@ class GUI( xbmcgui.WindowXML ):
             self.loadsettings()
             self.setupvars()
             self.setupcontrols()
+            self.buildemlist()
             xbmcgui.unlock()
         except:traceback.print_exc()
         
@@ -49,6 +58,23 @@ class GUI( xbmcgui.WindowXML ):
         except: control = 0
         if ( button_key == 'Keyboard ESC Button' or button_key == 'Back Button' or button_key == 'Remote Menu Button' ):
             self.close()
+        elif (50 <= focusid <= 59):
+            self.printEmail(self.getCurrentListPosition())
+
+    def printEmail(self, selected):
+        if self.emaillist[selected].is_multipart():
+            for part in self.emaillist[selected].walk():
+                if part.get_content_type() == "text/plain":
+                    self.getControl(67).setText(self.parse_email(part.get_payload()))
+                    break
+        else:self.getControl(67).setText(self.parse_email(self.emaillist[selected].get_payload()))
+
+    def parse_email(self, email):
+        parser = html2txt()
+        parser.reset()
+        parser.feed(email)
+        parser.close()
+        return parser.output()
     
     def setupcontrols(self):
         self.getControl(80).setLabel(self.inbox)
@@ -61,3 +87,46 @@ class GUI( xbmcgui.WindowXML ):
         w = Checkemail(ibsettings,inbox,self.account,self.language,False)
         w.checkemail()
         del w
+
+    def buildemlist(self):
+        if not exists(self.ibfolder + "emid.xib"):
+            self.addItem("Inbox Empty")
+            self.getControl(50).setEnabled(False)
+        else:
+            self.list = []
+            f = open(self.ibfolder + "emid.xib", "r")
+            for line in f.readlines():
+                theline = line.strip("\n")
+                myline = theline.split("|")
+                if myline[0] != "-":
+                    self.list.append(theline)
+            f.close()
+            if len(self.list) == 0:
+                self.addItem("Inbox Empty")
+                self.getControl(50).setEnabled(False)
+            else:self.createlist()
+
+    def createlist(self):
+        self.emaillist = []
+        for i,item in enumerate(self.list):
+            myitem = item.split("|")
+            f = open(self.ibfolder + myitem[2] + ".sss", "r")
+            myemail = email.message_from_string(f.read())
+            self.emaillist.insert(0,myemail)
+            f.close()
+            self.addItem(xbmcgui.ListItem(myemail.get('subject'),myemail.get('From'),"XBemailnotread.png","XBemailnotread.png"),0)
+
+class html2txt(SGMLParser):
+    def reset(self):
+        SGMLParser.reset(self)
+        self.pieces = []
+
+    def handle_data(self, text):
+        self.pieces.append(text)
+
+    def handle_entityref(self, ref):
+        if ref=='amp':
+            self.pieces.append("&")
+
+    def output(self):
+        return " ".join(self.pieces)        
