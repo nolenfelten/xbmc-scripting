d41f7475f64800bfb48f902b1f449e3038b24c29
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-07-01 11:17:57 +0000

    Updated: Email Engine Updated: Inbox menu - if menu detects that user has changed pop server or account name then it will refresh the inbox.

--- XinBox/Src/lib/XinBox_AccountMenu.py
+++ XinBox/Src/lib/XinBox_AccountMenu.py
@@ -1,7 +1,7 @@
 
 
 
-import xbmcgui, xbmc, os, random,traceback
+import xbmcgui, xbmc, os, random
 import XinBox_Util
 import XinBox_InBoxMenu
 import XinBox_InfoDialog
@@ -174,17 +174,15 @@ class AccountSettings(xbmcgui.WindowXML):
             else:self.launchinboxmenu(inboxname)
             self.setFocusId(9000)
         elif ( controlID == 64):
-            try:
-                if self.defaultaccount:
-                        self.accountSettings.setSetting("Default Account",str(self.defaultaccount))
-                        self.editallaccounts("Default Account","False",self.account)
-                self.theSettings.saveXMLfromArray()
-                self.savedaccountname = self.account
-                self.getControl(81).setLabel(self.account)
-                self.builddirs()
-                self.newaccount = False
-                self.launchinfo(48,"",self.language(49))
-            except:traceback.print_exc()
+            if self.defaultaccount:
+                    self.accountSettings.setSetting("Default Account",str(self.defaultaccount))
+                    self.editallaccounts("Default Account","False",self.account)
+            self.theSettings.saveXMLfromArray()
+            self.savedaccountname = self.account
+            self.getControl(81).setLabel(self.account)
+            self.builddirs()
+            self.newaccount = False
+            self.launchinfo(48,"",self.language(49))
         elif ( controlID == 65):
             if self.launchinfo(79,"",self.language(77)):
                 self.close()
