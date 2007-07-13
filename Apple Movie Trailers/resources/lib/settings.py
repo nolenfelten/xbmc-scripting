83d7921b305f718845e782e5b7e745182c8e4159
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-07-13 13:16:02 +0000

    fixed: [Apple Movie Trailers] Exclusives and Newest wasn't localized. changed: [Apple Movie Trailers] Finnish language file, thanks to Mikko

--- Apple Movie Trailers/resources/lib/settings.py
+++ Apple Movie Trailers/resources/lib/settings.py
@@ -84,7 +84,7 @@ class GUI( xbmcgui.WindowXMLDialog ):
         self.startup_categories = {}
         self.startup_titles = []
         for count, genre in enumerate( self.genres ):
-            self.startup_categories[ count ] = str( genre.title )
+            self.startup_categories[ count ] = str( genre.title.replace( "Newest", _( 150 ) ).replace( "Exclusives", _( 151 ) ) )
         self.startup_categories[ FAVORITES ] = _( 152 )
         self.startup_categories[ DOWNLOADED ] = _( 153 )
         self.startup_categories[ HD_TRAILERS ] = _( 160 )
@@ -92,14 +92,14 @@ class GUI( xbmcgui.WindowXMLDialog ):
         self.startup_categories[ CUSTOM_SEARCH ] = _( 162 )
         self.startup_categories[ WATCHED ] = _( 163 )
         for title in self.startup_categories.values():
-            self.startup_titles += [ title.replace( "Newest", _( 150 ) ).replace( "Exclusives", _( 151 ) ) ]
+            self.startup_titles += [ title ]
         self.startup_titles.sort()
 
     def _setup_thumbnail_display( self ):
         self.thumbnail = ( _( 2050 ), _( 2051 ), _( 2052 ), )
         
     def _setup_playback_mode( self ):
-        self.mode = ( _( 2030 ), _( 2031 ), "%s (%s)" % ( _( 2032 ), ( 84 ), ), "%s (%s)" % ( _( 2032 ), _( 86 ), ), )
+        self.mode = ( _( 2030 ), _( 2031 ), "%s (%s)" % ( _( 2032 ), _( 84 ), ), "%s (%s)" % ( _( 2032 ), _( 86 ), ), )
     
     def _setup_trailer_quality( self ):
         self.quality = ( _( 2020 ), _( 2021 ), _( 2022 ), "480p", "720p", "1080p" )
