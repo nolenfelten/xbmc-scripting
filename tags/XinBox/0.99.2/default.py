163d841d0606a088c37fa4d81968788fa370dfe5
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-08-12 05:31:58 +0000

    Copied remotely

---
+++ tags/XinBox/0.99.2/default.py
@@ -0,0 +1,43 @@
+      ##########################
+      #                        #                      
+      #         XinBox         #         
+      #     By Stanley87       #         
+      #                        #
+#######                        #######             
+#                                    #
+#                                    #
+#A pop3 + SMTP email client for XBMC #
+#                                    #
+######################################
+import sys, os, xbmc, time, xbmcgui
+from os.path import join, exists
+sys.path.append( os.path.join( sys.path[0], 'src', 'lib' ) )
+
+import XinBox_MainMenu
+from XinBox_Language import Language
+import XinBox_Util
+import threading
+
+scriptpath = os.getcwd().replace(";","")+"\\"
+
+lang = Language(XinBox_Util.__scriptname__)
+lang.load(scriptpath + "\\src\\language")
+_ = lang.string
+
+
+if __name__ == '__main__':
+        if exists("X:\\mmrunning.xib"):
+                dialog = xbmcgui.DialogProgress()
+                dialog.create(_(351), _(352))
+                if exists("X:\\mmcomu.xib"):
+                        os.remove("X:\\mmcomu.xib")
+                while exists("X:\\mmrunning.xib"):time.sleep(0.1)
+                dialog.update(100,_(352))
+                dialog.close()
+                try:
+                        w = XinBox_MainMenu.GUI("XinBox_MainMenu.xml",scriptpath+ "src","DefaultSkin",bforeFallback=False,minimode=sys.argv[1:][0],lang=_)
+                except:w = XinBox_MainMenu.GUI("XinBox_MainMenu.xml",scriptpath + "src","DefaultSkin",lang=_)
+        else:w = XinBox_MainMenu.GUI("XinBox_MainMenu.xml",scriptpath + "src","DefaultSkin",lang=_)
+        w.doModal()
+        del w
+
