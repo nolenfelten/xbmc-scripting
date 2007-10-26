0c27d0df07c61807b117173b2a762390df2f2aac
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2007-10-26 10:44:34 +0000

    Copied remotely

---
+++ tags/XinBox/0.99.91/Src/lib/XinBox_UpdateTemplate.py
@@ -0,0 +1,18 @@
+import xbmc, xbmcgui, sys, os, time
+origpath =
+newpath =
+
+for root, dirs, files in os.walk(origpath, topdown=False):
+    for name in files:
+        os.remove(os.path.join(root, name))
+    for name in dirs:
+        os.rmdir(os.path.join(root, name))
+os.rmdir(origpath)
+os.rename(newpath,origpath)
+try:
+    sys.path.append( os.path.join( origpath, 'src', 'lib' ) )
+    from XinBox_Util import UpdateSettings
+    UpdateSettings().loadsettings()
+    time.sleep(1)
+except:pass
+xbmc.executescript(origpath + "\\default.py")
