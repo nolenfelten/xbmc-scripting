a6def62c0429ed792faa1990541ba7d953bb437f
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-01-01 15:37:49 +0000

    [Plugin Maker] - changed: better support for rar files - changed: All English language strings

--- trunk/Plugin Maker/default.py
+++ trunk/Plugin Maker/default.py
@@ -18,9 +18,12 @@ __svn_revision__ = 0
 
 class Main:
     def __init__( self ):
-        path = self.get_browse_dialog( heading="Browse for video directory" )
+        # user selects plugin type
+        plugin_type = 0##xbmcgui.Dialog().yesno( __scriptname__, "What type of plugin is this?", "", "", "Video", "Music" )
+        # browse for the directory
+        path = self.get_browse_dialog( heading="Browse for %s directory" % ( ( "video", "music", )[ plugin_type ], ) )
         if ( path ):
-            self.create_plugin( path )
+            self.create_plugin( path, ( "video", "music", )[ plugin_type ] )
 
     def get_browse_dialog( self, default="", heading="", dlg_type=0, shares="files", mask="", use_thumbs=False, treat_as_folder=False ):
         """ shows a browse dialog and returns a value
@@ -33,20 +36,20 @@ class Main:
         value = dialog.browse( dlg_type, heading, shares, mask, use_thumbs, treat_as_folder, default )
         return value
 
-    def create_plugin( self, path ):
+    def create_plugin( self, path, plugin_type ):
         plugin = os.path.join( os.getcwd().replace( ";", "" ), "plugin.py" )
         plugin_name = self.get_keyboard( default=os.path.basename( path[ : -1 ] ) + " plugin", heading="Enter plugin name" )
-        plugin_path = os.path.join( "Q:\\plugins", "video", plugin_name )
+        plugin_path = os.path.join( "Q:\\plugins", plugin_type, plugin_name )
         if ( not os.path.isdir( plugin_path ) ):
             os.makedirs( plugin_path )
         try:
             # TODO: copy a generic thumb also or browse for thumb
             shutil.copy( plugin, os.path.join( plugin_path, "default.py" ) )
             shutil.copytree( os.path.join( os.getcwd().replace( ";", "" ), "resources" ), os.path.join( plugin_path, "resources" ) )
-            ok = self.edit_videopath( os.path.join( plugin_path, "resources", "settings.xml" ), path )
+            ok = self.edit_path( os.path.join( plugin_path, "resources", "settings.xml" ), path, plugin_type )
             if ( not ok ): raise
         except:
-            ok = xbmcgui.Dialog().ok( "Plugin Maker", "Copying plugin to new plugin folder failed!" )
+            ok = xbmcgui.Dialog().ok( "__scriptname__", "Copying plugin to new plugin folder failed!" )
 
     def get_keyboard( self, default="", heading="", hidden=False ):
         """ shows a keyboard and returns a value """
@@ -56,16 +59,16 @@ class Main:
             return keyboard.getText()
         return default
 
-    def edit_videopath( self, filepath, sourcepath ):
+    def edit_path( self, filepath, sourcepath, plugin_type ):
         try:
             ok = True
-            text = """<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n<settings>\n\t<setting id="path" type="folder" source="video" label="30000" default="%s" />\n</settings>""" % ( sourcepath, )
+            text = """<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n<settings>\n\t<setting id="path" type="folder" source="%s" label="30000" default="%s" />\n<setting id="content" type="enum" lvalues="30011|30012|30013" label="30010" default="%d" />\n</settings>""" % ( plugin_type, sourcepath, ( 0, 1, )[ plugin_type == "music" ], )
             # write new .py file
             file_object = open( filepath, "w" )
             file_object.write( text )
             file_object.close()
         except:
-            ok = xbmcgui.Dialog().ok( "Plugin Maker", "Creating settings.xml file failed!" )
+            ok = xbmcgui.Dialog().ok( "__scriptname__", "Creating settings.xml file failed!" )
         return ok
 
 
