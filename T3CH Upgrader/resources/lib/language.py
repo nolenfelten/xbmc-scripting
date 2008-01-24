5496e19120a476905440ff683cef0922bf588d33
bigbellybilly <bigbellybilly@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-01-24 14:36:14 +0000

    New: view script readme/Changelog.txt Updated language.py

--- T3CH Upgrader/resources/lib/language.py
+++ T3CH Upgrader/resources/lib/language.py
@@ -2,7 +2,7 @@
 Language module
 
 Nuka1195
-05/01/2008 Some changes by BigBellyBilly
+24/01/2008 Some changes by BigBellyBilly
 """
 
 import os, sys
@@ -11,24 +11,31 @@ import xml.dom.minidom
 
 class Language:
     """ Language Class: creates a dictionary of localized strings { int: string } """
+    LANGUAGE_DIR_NAME = 'language'
+    STRINGS_FILENAME = 'strings.xml'
+    DEFAULT_LANG = 'english'
+    
     def __init__( self ):
         """ initializer """
         # language folder
-        module_dir = os.path.dirname( sys.modules['language'].__file__ )
-        base_path = os.path.join( os.path.dirname( module_dir ), 'language' )
-
+        base_path = self.get_base_path()
         # get the current language
         language = self._get_language( base_path )
         # create strings dictionary
         self._create_localized_dict( base_path, language )
-        
+
+    def get_base_path( self ):
+        """ returns full language base path """
+        module_dir = os.path.dirname( sys.modules['language'].__file__ )
+        return os.path.join( os.path.dirname( module_dir ), Language.LANGUAGE_DIR_NAME )
+
     def _get_language( self, base_path ):
         """ returns the current language if a strings.xml file exists else returns english """
         # get the current users language setting
         language = xbmc.getLanguage().lower()
         # if no strings.xml file exists, default to english
-        if ( not os.path.isfile( os.path.join( base_path, language, "strings.xml" ) ) ):
-            language = "english"
+        if ( not os.path.isfile( os.path.join( base_path, language, Language.STRINGS_FILENAME ) ) ):
+            language = Language.DEFAULT_LANG
         return language
 
     def _create_localized_dict( self, base_path, language ):
@@ -36,10 +43,10 @@ class Language:
         # localized strings dictionary
         self.strings = {}
         # add localized strings
-        self._parse_strings_file( os.path.join( base_path, language, "strings.xml" ) )
+        self._parse_strings_file( os.path.join( base_path, language, Language.STRINGS_FILENAME ) )
         # fill-in missing strings with english strings
-        if ( language != "english" ):
-            self._parse_strings_file( os.path.join( base_path, "english", "strings.xml" ) )
+        if ( language != Language.DEFAULT_LANG ):
+            self._parse_strings_file( os.path.join( base_path, Language.DEFAULT_LANG, Language.STRINGS_FILENAME ) )
         
     def _parse_strings_file( self, language_path ):
         """ adds localized strings to self.strings dictionary """
