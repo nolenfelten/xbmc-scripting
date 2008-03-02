0cd2626b7e43b1891b7fed0bda8e922963f952b3
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-02 23:31:41 +0000

    removed: file viewing code from example script

--- Script Templates/example/resources/lib/main.py
+++ Script Templates/example/resources/lib/main.py
@@ -16,23 +16,8 @@ class ExampleScriptWindow( common.gui.BaseScriptWindow ):
             101: {
                 'onClick': self.close,
             },
-            # BUTTON: open
-            102: {
-                'onClick': self.open_new_file,
-            },
         }
         common.gui.BaseScriptWindow.__init__( self, xmlFile, resourcePath )
-    def open_new_file( self ):
-        # get a path to the file 
-        filepath = common.gui.get_browse_dialog( heading = common.localize( 1001 ) )
-        filehandle = open( filepath, 'r' )
-        # clear the list before adding new items
-        self.clearList()
-        # add a new item to the list for each line of text in the file
-        for line in filehandle:
-            self.addItem( line )
-        # close the file
-        filehandle.close()
 
 xmlFile = 'script-%s-window.xml' % common.scriptname.replace( ' ', '_' )
 window = ExampleScriptWindow( xmlFile, common.resource_path )
