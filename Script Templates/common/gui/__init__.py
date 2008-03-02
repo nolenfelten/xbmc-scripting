ea950f90d919a8f966f68af1c01983e214486c61
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-02 18:03:13 +0000

    added nuka1195's excellent helper functions: get_keyboard, get_numeric_dialog, get_browse_dialog

--- Script Templates/common/gui/__init__.py
+++ Script Templates/common/gui/__init__.py
@@ -1,7 +1,7 @@
 # official python modules
 import os, sys
 # official xbmc modules
-import xbmcgui
+import xbmc, xbmcgui
 # script modules
 import codes
 
@@ -9,6 +9,38 @@ import codes
 common = sys.modules['common']
 # language localization 'macro'
 _ = common.localize
+
+def get_keyboard( default = '', heading = '', hidden = False ):
+    '''Shows a keyboard and returns a value'''
+    keyboard = xbmc.Keyboard( default, heading, hidden )
+    keyboard.doModal()
+    if ( keyboard.isConfirmed() ):
+        return keyboard.getText()
+    return default
+
+def get_numeric_dialog( default = '', heading = '', dlg_type = 3 ):
+    '''
+    Shows a numeric dialog and returns a value
+        - 0 : ShowAndGetNumber		(default format: #)
+        - 1 : ShowAndGetDate			(default format: DD/MM/YYYY)
+        - 2 : ShowAndGetTime			(default format: HH:MM)
+        - 3 : ShowAndGetIPAddress	(default format: #.#.#.#)
+    '''
+    dialog = xbmcgui.Dialog()
+    value = dialog.numeric( type, heading, default )
+    return value
+
+def get_browse_dialog( default = '', heading = '', dlg_type = 1, shares = 'files', mask = '', use_thumbs = False, treat_as_folder = False ):
+    '''
+    Shows a browse dialog and returns a value
+        - 0 : ShowAndGetDirectory
+        - 1 : ShowAndGetFile
+        - 2 : ShowAndGetImage
+        - 3 : ShowAndGetWriteableDirectory
+    '''
+    dialog = xbmcgui.Dialog()
+    value = dialog.browse( dlg_type, heading, shares, mask, use_thumbs, treat_as_folder, default )
+    return value
 
 class BaseScriptWindow( xbmcgui.WindowXML ):
     def __init__( self, xmlFile = '', resourcePath = '', defaultName = 'Default', forceFallback = False ):
