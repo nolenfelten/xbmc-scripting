3f2891188d29ef1cb1ad9a276929969f759b2cd5
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-06 04:44:12 +0000

    removed: id 1 from strings.xml added: id 102 to strings.xml removed: PM3 skin until gui layout is finalized added: Save button that does nothing right now added: new layout for Default skin fixed: bug when the open file dialog was closed without selecting a file

--- Scratcher/resources/lib/main.py
+++ Scratcher/resources/lib/main.py
@@ -8,34 +8,38 @@ import common
 class ScriptWindow( common.gui.BaseScriptWindow ):
     def __init__( self, xmlFile, resourcePath ):
         self.controls_map = {
-            2: { # LABEL: script name
+            1: { # LABEL: script name
                 'label': common.scriptname,
             },
             101: { # BUTTON: open
                 'onClick': self.open_new_file,
+            },
+            102: { # BUTTON: save
+                'onClick': None,
             },
         }
         common.gui.BaseScriptWindow.__init__( self, xmlFile, resourcePath )
     def open_new_file( self ):
         # get a path to the file 
         filepath = common.gui.show_browse_dialog( heading = common.localize( 1001 ) )
-        filehandle = open( filepath, 'r' )
-        # clear the list before adding new items
-        self.clearList()
-        # function to beautify each line of text for display
-        def line_cleanup( line ):
-            end_of_line_chars = [ '\r', '\n' ]
-            for char in end_of_line_chars: line = line.replace( char, '' )
-            return line
-        # add a new item to the list for each line of text in the file
-        line_number = 0
-        for line in filehandle:
-            line = line_cleanup( line )
-            line_number = line_number + 1
-            item = xbmcgui.ListItem( line, str( line_number ) )
-            self.addItem( item )
-        # close the file
-        filehandle.close()
+        if len( filepath ):
+            filehandle = open( filepath, 'r' )
+            # clear the list before adding new items
+            self.clearList()
+            # function to beautify each line of text for display
+            def line_cleanup( line ):
+                end_of_line_chars = [ '\r', '\n' ]
+                for char in end_of_line_chars: line = line.replace( char, '' )
+                return line
+            # add a new item to the list for each line of text in the file
+            line_number = 0
+            for line in filehandle:
+                line = line_cleanup( line )
+                line_number = line_number + 1
+                item = xbmcgui.ListItem( line, str( line_number ) )
+                self.addItem( item )
+            # close the file
+            filehandle.close()
 
 # construct the xml filename
 xmlFile = 'script-%s-window.xml' % common.scriptname.replace( ' ', '_' )
