b39e63dd73f03f16eb164d6fb0d498fa2ddfc9f6
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-07 01:01:16 +0000

    changed: show_keyboard, show_numeric_dialog, and show_browse_dialog were moved into a new dialog.py file, callable through common.gui.dialog.keyboard, common.gui.dialog.numeric, and common.gui.dialog.browse

---
+++ trunk/Script Templates/common/gui/dialog.py
@@ -0,0 +1,34 @@
+# official xbmc modules
+import xbmc, xbmcgui
+
+def keyboard( default = '', heading = '', hidden = False ):
+    '''Shows a keyboard and returns a value'''
+    keyboard = xbmc.Keyboard( default, heading, hidden )
+    keyboard.doModal()
+    if ( keyboard.isConfirmed() ):
+        return keyboard.getText()
+    return default
+
+def numeric( default = '', heading = '', dlg_type = 3 ):
+    '''
+    Shows a numeric dialog and returns a value
+        - 0 : ShowAndGetNumber		(default format: #)
+        - 1 : ShowAndGetDate			(default format: DD/MM/YYYY)
+        - 2 : ShowAndGetTime			(default format: HH:MM)
+        - 3 : ShowAndGetIPAddress	(default format: #.#.#.#)
+    '''
+    dialog = xbmcgui.Dialog()
+    value = dialog.numeric( type, heading, default )
+    return value
+
+def browse( default = '', heading = '', dlg_type = 1, shares = 'files', mask = '', use_thumbs = False, treat_as_folder = False ):
+    '''
+    Shows a browse dialog and returns a value
+        - 0 : ShowAndGetDirectory
+        - 1 : ShowAndGetFile
+        - 2 : ShowAndGetImage
+        - 3 : ShowAndGetWriteableDirectory
+    '''
+    dialog = xbmcgui.Dialog()
+    value = dialog.browse( dlg_type, heading, shares, mask, use_thumbs, treat_as_folder, default )
+    return value
