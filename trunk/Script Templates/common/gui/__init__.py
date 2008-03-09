d3d493377e862a2c9536c8ff57ad4f68aaa5c09f
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-09 08:45:59 +0000

    added: moved code dealing with WindowXML and WindowXMLDialog into base.py changed: code that was moved into base.py was changed significantly to ease writing of dependent code changed: simplified BaseScriptWindow and BaseScriptDialog classes by changing them to support the code in base.py

--- trunk/Script Templates/common/gui/__init__.py
+++ trunk/Script Templates/common/gui/__init__.py
@@ -1,41 +1,13 @@
 # official python modules
-import os, sys
-# official xbmc modules
-import xbmc, xbmcgui
+import sys
 # script modules
-import codes, dialog
+import base, dialog
 
 # script variables
 common = sys.modules['common']
-# language localization 'macro'
-_ = common.localize
 
-class BaseScriptWindow( xbmcgui.WindowXML ):
+class BaseScriptWindow:
     def __init__( self, xmlFile = '', resourcePath = '', defaultName = 'Default', forceFallback = False ):
-        '''
-            At the time of this writing, the xbmcgui.WindowXML class does not
-                follow python conventions in handling it's subclasses. The
-                equivalent xbmcgui.WindowXML.__init__() method seems to be 
-                evaluated prior to evaluating the code contained here, meaning
-                that this BaseScriptWindow class can not modify the required
-                arguments for it's invokation. Hopefully this will be fixed!
-
-            For now, however, this means that the automatic generation of the
-                xml filename and resource path here will not allow code that
-                subclasses BaseScriptWindow to ignore passing these arguments.
-                So, even though the arguments above show defaults of empty
-                strings for xmlFile and resourcePath, all subclasses must pass
-                a proper xml filename and the correct resource path:
-
-                    BaseScriptWindow( xmlFile, resourcePath )
-
-            The code to automatically generate these arguments has been left 
-                here for demonstration purposes, and in the hopes that if 
-                xbmcgui.WindowXML ever gets fixed, this code will already be
-                here, and subclasses can use the intended syntax:
-
-                    BaseScriptWindow()
-        '''
         if not len( xmlFile ):
             xmlFile = 'script-%s-window.xml' % common.scriptname.replace( ' ', '_' )
         if not len( resourcePath ):
@@ -48,99 +20,18 @@ class BaseScriptWindow( xbmcgui.WindowXML ):
                 #   'label': str(),
                 # },
             }
-        xbmcgui.WindowXML.__init__( self, xmlFile, resourcePath, defaultName, forceFallback )
- 
-    def onInit( self ):
-        '''
-            sets each control to it's corresponding label from strings.xml
-        '''
-        for id, callbacks in self.controls_map.iteritems():
-            try:
-                # get the control object for this id
-                control = self.getControl( id )
-                try:
-                    # see if there is a label set in the code
-                    #   callback is a string or variable (see below) 
-                    #   for the label
-                    callback = callbacks['label']
-                except: callback = None
-                if callback:
-                    try:
-                        # there may be data in strings.xml also, check with
-                        #   a strict (error throwing) lookup
-                        label = _( id, strict = True )
-                    except:
-                        # nothing in strings.xml
-                        label = ''
-                    try:
-                        # try to use the data from strings.xml as a format for
-                        #   assigning data from the callback variable
-                        label = label % callback
-                    except:
-                        # if that doesn't work, treat callback as a string and
-                        #   concatenate to the data from strings.xml
-                        label = label + callback
-                else:
-                    # no label information was set in the code, so use the 
-                    #   strings.xml lookup alone
-                    label = _( id )
-                if hasattr( control, 'setLabel' ):
-                    # make sure that this control can even accept a label!
-                    control.setLabel( label )
-                # explicitly delete the control object for the next loop pass
-                del control
-            except:
-                # something horrible occurred
-                import traceback
-                print '-' * 79
-                print 'error setting label for', id
-                traceback.print_exc()
- 
-    def onAction( self, action ):
-        for i in [ action.getId(), action.getButtonCode() ]:
-            # this would work nicer here in standard python, but xbmc has 
-            #   problems with it
-            # if i in codes.EXIT_SCRIPT:
-            for j in codes.EXIT_SCRIPT:
-                if i == j:
-                    self.close()
-                    break
+        self.window = base.__internal_base_classWindow__( xmlFile, resourcePath, defaultName, forceFallback, parentClass = self )
 
-    def onClick( self, controlID ):
-        for id, callbacks in self.controls_map.iteritems():
-            if controlID == id:
-                try:
-                    callback = callbacks['onClick']
-                except: callback = None
-                if callback: callback()
- 
-    def onFocus( self, controlID ):
-        for id, callbacks in self.controls_map.iteritems():
-            if controlID == id:
-                try:
-                    callback = callbacks['onFocus']
-                except: callback = None
-                if callback: callback()
+    def close( self ):
+        self.window.close()
 
-class BaseScriptDialog( xbmcgui.WindowXMLDialog ):
+    def show( self ):
+        self.window.doModal()
+
+class BaseScriptDialog:
     def __init__( self, xmlFile = '', resourcePath = '', defaultName = 'Default', forceFallback = False ):
-        '''
-            Even though the arguments above show defaults of empty strings for 
-                xmlFile and resourcePath, all subclasses must pass a proper 
-                xml filename and the correct resource path:
-
-                    BaseScriptDialog( xmlFile, resourcePath )
-
-            The code to automatically generate these arguments has been left 
-                here for demonstration purposes, and in the hopes that in the 
-                future, subclasses can use the intended syntax:
-
-                    BaseScriptDialog()
-
-            Note: see docstring for BaseScriptWindow for further discussion.
-        '''
         if not len( xmlFile ):
-            xmlFile = 'script-%s-window.xml' % common.scriptname.replace( ' ', '_' )
+            xmlFile = 'script-%s-dialog.xml' % common.scriptname.replace( ' ', '_' )
         if not len( resourcePath ):
             resourcePath = common.resource_path
         if not hasattr( self, 'controls_map' ):
@@ -151,76 +42,10 @@ class BaseScriptDialog( xbmcgui.WindowXMLDialog ):
                 #   'label': str(),
                 # },
             }
-        xbmcgui.WindowXMLDialog.__init__( self, xmlFile, resourcePath, defaultName, forceFallback )
+        self.window = base.__internal_base_classDialog__( xmlFile, resourcePath, defaultName, forceFallback, parentClass = self )
 
-    def onInit( self ):
-        '''
-            sets each control to it's corresponding label from strings.xml
-        '''
-        for id, callbacks in self.controls_map.iteritems():
-            try:
-                # get the control object for this id
-                control = self.getControl( id )
-                try:
-                    # see if there is a label set in the code
-                    #   callback is a string or variable (see below) 
-                    #   for the label
-                    callback = callbacks['label']
-                except: callback = None
-                if callback:
-                    try:
-                        # there may be data in strings.xml also, check with
-                        #   a strict (error throwing) lookup
-                        label = _( id, strict = True )
-                    except:
-                        # nothing in strings.xml
-                        label = ''
-                    try:
-                        # try to use the data from strings.xml as a format for
-                        #   assigning data from the callback variable
-                        label = label % callback
-                    except:
-                        # if that doesn't work, treat callback as a string and
-                        #   concatenate to the data from strings.xml
-                        label = label + callback
-                else:
-                    # no label information was set in the code, so use the 
-                    #   strings.xml lookup alone
-                    label = _( id )
-                if hasattr( control, 'setLabel' ):
-                    # make sure that this control can even accept a label!
-                    control.setLabel( label )
-                # explicitly delete the control object for the next loop pass
-                del control
-            except:
-                # something horrible occurred
-                import traceback
-                print '-' * 79
-                print 'error setting label for', id
-                traceback.print_exc()
- 
-    def onAction( self, action ):
-        for i in [ action.getId(), action.getButtonCode() ]:
-            # this would work nicer here in standard python, but xbmc has 
-            #   problems with it
-            # if i in codes.EXIT_SCRIPT:
-            for j in codes.EXIT_SCRIPT:
-                if i == j:
-                    self.close()
-                    break
+    def close( self ):
+        self.window.close()
 
-    def onClick( self, controlID ):
-        for id, callbacks in self.controls_map.iteritems():
-            if controlID == id:
-                try:
-                    callback = callbacks['onClick']
-                except: callback = None
-                if callback: callback()
- 
-    def onFocus( self, controlID ):
-        for id, callbacks in self.controls_map.iteritems():
-            if controlID == id:
-                try:
-                    callback = callbacks['onFocus']
-                except: callback = None
-                if callback: callback()
+    def show( self ):
+        self.window.doModal()
