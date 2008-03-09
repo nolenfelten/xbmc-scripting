c17c4e97dc7f40dce89a076f8c911f45fe5f17ea
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-09 05:18:55 +0000

    added: BaseScriptDialog class; provides same functionality as BaseScriptWindow class

--- Script Templates/common/gui/__init__.py
+++ Script Templates/common/gui/__init__.py
@@ -121,3 +121,106 @@ class BaseScriptWindow( xbmcgui.WindowXML ):
                     callback = callbacks['onFocus']
                 except: callback = None
                 if callback: callback()
+
+class BaseScriptDialog( xbmcgui.WindowXMLDialog ):
+    def __init__( self, xmlFile = '', resourcePath = '', defaultName = 'Default', forceFallback = False ):
+        '''
+            Even though the arguments above show defaults of empty strings for 
+                xmlFile and resourcePath, all subclasses must pass a proper 
+                xml filename and the correct resource path:
+
+                    BaseScriptDialog( xmlFile, resourcePath )
+
+            The code to automatically generate these arguments has been left 
+                here for demonstration purposes, and in the hopes that in the 
+                future, subclasses can use the intended syntax:
+
+                    BaseScriptDialog()
+
+            Note: see docstring for BaseScriptWindow for further discussion.
+        '''
+        if not len( xmlFile ):
+            xmlFile = 'script-%s-window.xml' % common.scriptname.replace( ' ', '_' )
+        if not len( resourcePath ):
+            resourcePath = common.resource_path
+        if not hasattr( self, 'controls_map' ):
+            self.controls_map = {
+                # id: {
+                #   'onClick': callback,
+                #   'onFocus': callback,
+                #   'label': str(),
+                # },
+            }
+        xbmcgui.WindowXMLDialog.__init__( self, xmlFile, resourcePath, defaultName, forceFallback )
+
+    def onInit( self ):
+        '''
+            sets each control to it's corresponding label from strings.xml
+        '''
+        for id, callbacks in self.controls_map.iteritems():
+            try:
+                # get the control object for this id
+                control = self.getControl( id )
+                try:
+                    # see if there is a label set in the code
+                    #   callback is a string or variable (see below) 
+                    #   for the label
+                    callback = callbacks['label']
+                except: callback = None
+                if callback:
+                    try:
+                        # there may be data in strings.xml also, check with
+                        #   a strict (error throwing) lookup
+                        label = _( id, strict = True )
+                    except:
+                        # nothing in strings.xml
+                        label = ''
+                    try:
+                        # try to use the data from strings.xml as a format for
+                        #   assigning data from the callback variable
+                        label = label % callback
+                    except:
+                        # if that doesn't work, treat callback as a string and
+                        #   concatenate to the data from strings.xml
+                        label = label + callback
+                else:
+                    # no label information was set in the code, so use the 
+                    #   strings.xml lookup alone
+                    label = _( id )
+                if hasattr( control, 'setLabel' ):
+                    # make sure that this control can even accept a label!
+                    control.setLabel( label )
+                # explicitly delete the control object for the next loop pass
+                del control
+            except:
+                # something horrible occurred
+                import traceback
+                print '-' * 79
+                print 'error setting label for', id
+                traceback.print_exc()
+ 
+    def onAction( self, action ):
+        for i in [ action.getId(), action.getButtonCode() ]:
+            # this would work nicer here in standard python, but xbmc has 
+            #   problems with it
+            # if i in codes.EXIT_SCRIPT:
+            for j in codes.EXIT_SCRIPT:
+                if i == j:
+                    self.close()
+                    break
+
+    def onClick( self, controlID ):
+        for id, callbacks in self.controls_map.iteritems():
+            if controlID == id:
+                try:
+                    callback = callbacks['onClick']
+                except: callback = None
+                if callback: callback()
+ 
+    def onFocus( self, controlID ):
+        for id, callbacks in self.controls_map.iteritems():
+            if controlID == id:
+                try:
+                    callback = callbacks['onFocus']
+                except: callback = None
+                if callback: callback()
