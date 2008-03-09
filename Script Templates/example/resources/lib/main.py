148b519195319d0c738eca436a2acc816a2e9682
killarny <killarny@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-09 17:12:27 +0000

    added: button for a popup menu (menu not implemented yet) changed: rearranged and streamlined code

--- Script Templates/example/resources/lib/main.py
+++ Script Templates/example/resources/lib/main.py
@@ -6,18 +6,19 @@ import common
 class ExampleScriptWindow( common.gui.BaseScriptWindow ):
     def __init__( self ):
         self.controls_map = {
-            # LABEL: xbox media center
-            1: None,
-            # LABEL: script name
-            2: {
+            1: { # LABEL: script name
                 'label': common.scriptname,
             },
-            # BUTTON: quit
-            101: {
+            101: { # BUTTON: show popup menu
+                'onClick': self.popup,
+            },
+            110: { # BUTTON: quit
                 'onClick': self.close,
             },
         }
         common.gui.BaseScriptWindow.__init__( self )
+    def popup( self ):
+        print 'popup goes here'
 
 window = ExampleScriptWindow()
 window.show()
