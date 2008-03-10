41f0f26e0f34739ac5a900032d337687ae99fb6b
bigbellybilly <bigbellybilly@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-10 00:35:28 +0000

    [Comics] v1.5 tag release

--- XinBox/Src/lib/XinBox_UpdateTemplate2.py
+++
@@ -1,8 +0,0 @@
-import os, xbmc, time
-origver =
-origpath = 
-newpath =
-
-os.rename(origpath, origpath + "(OLD)_v" + origver)
-os.rename(newpath,origpath)
-xbmc.executescript(origpath + "\\default.py")
