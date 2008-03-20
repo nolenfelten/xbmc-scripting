f1b113d2e79c068fbf018afe0c01e56706e75bff
bigbellybilly <bigbellybilly@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-03-20 14:38:52 +0000

    [Football] v1.4 tag release

--- Script Templates/example/default.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    import os
-    cwd = os.getcwd()
-    if cwd[-1] == ';': # XBMC erroneously puts a trailing semicolon on this value
-        cwd = cwd[:-1]
-    sys.path.insert( 0, os.path.join( cwd, 'resources', 'lib' ) )
-    import main
