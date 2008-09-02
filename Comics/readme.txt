d540b0fd3cee6cc5d90e054e8e2cec443a4b0e6c
bigbellybilly <bigbellybilly@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-09-02 13:10:32 +0000

    [myTV] v1.18 released

--- Comics/readme.txt
+++
@@ -1,2 +0,0 @@
-Full readme.txt can be found in resources\language\<language> (fallback to english if your language doesn't exist)
-View using Menu Menu within script.
--- Scratcher/default.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    import os
-    sys.path.insert( 0,
-        os.path.join( os.path.dirname(__file__), 'resources', 'lib' ) )
-    del os
-    import main
