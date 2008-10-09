1e603cd99af80541c12d02a64e748d8c59dad117
bigbellybilly <bigbellybilly@6ef6da06-101e-0410-bb12-11b2d28f535c>
2008-10-09 22:45:44 +0000

    [myTV] Removed update test

--- tags/myTV/1.0/autoexec.py
+++
@@ -1,7 +0,0 @@
-# Adding this code to your own autoexec.py will cause all previously
-# saved myTV Alarm Clocks to be setup on XBMC startup.
-
-import xbmc
-
-xbmc.executescript('q:\\scripts\\mytv\\AlarmClock.py')
-
