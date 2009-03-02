1832e97c5c35e31e2927317f6a2a62621b4cfe71
BigBellyBilly <BigBellyBilly@6ef6da06-101e-0410-bb12-11b2d28f535c>
2009-03-02 16:48:17 +0000

    [myTV] -delete: svn sync problems

--- myTV/autoexec.py
+++
@@ -1,7 +0,0 @@
-# Adding this code to your own autoexec.py will cause all previously
-# saved myTV Alarm Clocks to be setup on XBMC startup.
-
-import xbmc, os.path
-
-xbmc.executescript(xbmc.translatePath(os.path.join('Q:'+os.sep,'scripts','myTV','AlarmClock.py')))
-
