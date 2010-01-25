9e53ab24b8f9c9fcf95d990b3f535ca333c2b183
nuka1195 <nuka1195@6ef6da06-101e-0410-bb12-11b2d28f535c>
2010-01-25 17:17:30 +0000

    [Apple Movie Trailers]
    -changed: version -> pre-0.99.7.3b
    -fixed: script wanted to keep rebuilding database

--- Apple Movie Trailers/resources/lib/utilities.py
+++ Apple Movie Trailers/resources/lib/utilities.py
@@ -17,7 +17,7 @@ __version__ = sys.modules[ "__main__" ].__version__
 __svn_revision__ = sys.modules[ "__main__" ].__svn_revision__
 
 # comapatble versions
-DATABASE_VERSIONS = ( "pre-0.99.7.1", "pre-0.99.7.2", "pre-0.99.7.3", )
+DATABASE_VERSIONS = ( "pre-0.99.7.1", "pre-0.99.7.2", "pre-0.99.7.3", "pre-0.99.7.3a", "pre-0.99.7.3b", )
 SETTINGS_VERSIONS = DATABASE_VERSIONS
 # special categories
 GENRES = -1
