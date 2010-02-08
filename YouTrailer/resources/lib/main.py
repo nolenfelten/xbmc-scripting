f67c1659bf018f03f23d2204bca188ef8b2c4464
stanley87.matt <stanley87.matt@6ef6da06-101e-0410-bb12-11b2d28f535c>
2010-02-08 00:16:44 +0000

    Updated: Script now stores Youtube url's in a database to allow for future faster loading of trailers

--- YouTrailer/resources/lib/main.py
+++ YouTrailer/resources/lib/main.py
@@ -9,35 +9,60 @@ from cgi import parse_qs
 #enable language localization
 _lang = xbmc.Language(os.getcwd()).getLocalizedString
 
-dbpath = xbmc.translatePath( "special://database/MyVideos34.db" )
+DBPATH = xbmc.translatePath( "special://database/MyVideos34.db" )
 __scriptname__ = sys.modules[ "__main__" ].__scriptname__
-#BASE_DATABASE_PATH = os.path.join( xbmc.translatePath( "special://profile/" ), "script_data", __scriptname__, "YouTrailer.db" )
 
+BASE_DATABASE_PATH = os.path.join( xbmc.translatePath( "special://profile/" ), "script_data", __scriptname__, "YouTrailer.db" )
+
+SLEEP_TIME = 5
 
 class Main:
     def __init__(self):
-#        if ( not os.path.isdir( os.path.dirname( BASE_DATABASE_PATH ) ) ):
-#            os.makedirs( os.path.dirname( BASE_DATABASE_PATH ) )
         starttime = time.time()
         self.setupVariables()
+        self.buildDatabase()
         self.file_name = sys.argv[1].strip()
         self.file_path = sys.argv[2].strip()
-        if( self.fetchXbmcData() ):
-            self.main()
+        self.main()
         while time.time() - starttime < 5:
             pass
 
+    def buildDatabase( self ):
+        if ( not os.path.isdir( os.path.dirname( BASE_DATABASE_PATH ) ) ):
+            os.makedirs( os.path.dirname( BASE_DATABASE_PATH ) )
+        db = sqlite.connect( BASE_DATABASE_PATH )
+        cursor = db.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS trailers (id INTEGER PRIMARY KEY, name VARCHAR(100), imdb_id VARACHAR(25), tmdb_id VARACHAR(25), trailer_url VARACHAR(100) );')
+        db.commit()
+        db.close()
+        
     def setupVariables( self ):
         self.tmdb_api = tmdb.MovieDb()
         self.player = xbmc.Player()
         self.progress_dialog = xbmcgui.DialogProgress()
         self.dialog = xbmcgui.Dialog()
         self.full_url = ''
-        self.error_id = 34
+        self.imdb_id = ''
+        self.tmdb_id = ''
+        self.trailer_url = ''
+        self.error_id = 35
+
+    def main( self ):
+        try:
+            self.progress_dialog.create( __scriptname__, '' )
+            self.fetchXbmcData()
+            self.checkLocalTrailer()
+            self.playTrailer()
+            self.progress_dialog.close()
+            self.validChecker()
+        except:
+            self.progress_dialog.close()
+            self.dialog.ok( "%s - %s" % ( __scriptname__, _lang( 30 ) ), "", "", _lang( self.error_id ) )
+
 
     def fetchXbmcData( self ):
         try:
-            db = sqlite.connect( dbpath )
+            db = sqlite.connect( DBPATH )
             cursor = db.cursor()
             cursor.execute( "SELECT idPath FROM path WHERE strPath = ?;" , ( self.file_path, ) )
             idPath = cursor.fetchone()[0]
@@ -50,20 +75,10 @@ class Main:
             self.movie_genre = xbmc_data[2].strip()
             self.trailer_url = xbmc_data[3].strip()
             db.close()
-            return True
         except:
-            self.dialog.ok( __scriptname__, _lang( 30 ), "", _lang( 31 ) )
-            return False
+            self.error_id = 31
+            raise
 
-    def main( self ):
-        try:
-            self.progress_dialog.create( __scriptname__, self.movie_name )
-            self.checkLocalTrailer()
-            self.playTrailer()
-            self.progress_dialog.close()
-        except:
-            self.progress_dialog.close()
-            self.dialog.ok( "%s - %s" % ( __scriptname__, _lang( 30 ) ), '%s %s' % ( self.movie_name, _lang( 2 ) ), "", _lang( self.error_id ) )
 
     def checkLocalTrailer( self ):
         if( ( not "http" in self.trailer_url ) and ( xbmc.executehttpapi( 'FileExists(%s)' % self.trailer_url ) == '<li>True' ) ):
@@ -78,10 +93,15 @@ class Main:
             self.checkDb()
              
     def checkDb( self ):
-        #query scripts db looking for youtubeurl
-        #checking here, returning no result simulated
-        result = None
-        if( result == None ):
+        try:
+            db = sqlite.connect( BASE_DATABASE_PATH )
+            cursor = db.cursor()
+            cursor.execute( "SELECT trailer_url FROM trailers WHERE imdb_id = ?;" , ( self.imdb_id, ) )
+            self.trailer_url = cursor.fetchone()[0]
+            if( self.trailer_url == '' ):
+                raise
+            self.fetchToken()
+        except:
             self.fetchUrl()
             self.fetchToken()
 
@@ -116,11 +136,38 @@ class Main:
             raise
         
     def playTrailer( self ):
-        self.progress_dialog.update( 100, '%s %s' % ( self.movie_name, _lang( 2 ) ), "", _lang( 14 ) )
-        cache_name = xbmc.getCacheThumbName( os.path.join( self.file_path, self.file_name ) )
-        thumb_image = os.path.join(  xbmc.translatePath( "special://thumbnails/Video/" ), cache_name[0],  cache_name )
-        listitem = xbmcgui.ListItem( self.movie_name, thumbnailImage = thumb_image )
-        listitem.setInfo( 'video', {'Title': '%s %s' % ( self.movie_name, _lang( 2 ) ), 'Studio' : __scriptname__, 'Genre' : self.movie_genre } )
-        self.player.play( self.full_url, listitem )
+        try:
+            self.progress_dialog.update( 100, '%s %s' % ( self.movie_name, _lang( 2 ) ), "", _lang( 14 ) )
+            cache_name = xbmc.getCacheThumbName( os.path.join( self.file_path, self.file_name ) )
+            thumb_image = os.path.join(  xbmc.translatePath( "special://thumbnails/Video/" ), cache_name[0],  cache_name )
+            listitem = xbmcgui.ListItem( self.movie_name, thumbnailImage = thumb_image )
+            listitem.setInfo( 'video', {'Title': '%s %s' % ( self.movie_name, _lang( 2 ) ), 'Studio' : __scriptname__, 'Genre' : self.movie_genre } )
+            self.player.play( self.full_url, listitem )
+        except:
+            self.error_id = 34
+            raise
 
+    def validChecker( self ):
+        for i in range(0, SLEEP_TIME):
+            if( self.player.isPlayingVideo() ):
+                if ( self.player.getPlayingFile() == self.full_url ):
+                    self.addDb()
+                    break
+            time.sleep( 1 )
+
+    def addDb( self ):
+        try:
+            db = sqlite.connect( BASE_DATABASE_PATH )
+            cursor = db.cursor()
+            cursor.execute( "SELECT id FROM trailers WHERE imdb_id = ?;" , ( self.imdb_id, ) )
+            result = cursor.fetchone()
+            if( result != None ):
+                cursor.execute( "UPDATE trailers SET name=?, tmdb_id=?, trailer_url=? WHERE id='%s'" % result[0], ( self.movie_name, self.tmdb_id, self.trailer_url ) )
+            else:
+                cursor.execute('INSERT INTO trailers ( name, imdb_id, tmdb_id, trailer_url ) VALUES ( ?, ?, ?, ? );', ( self.movie_name, self.imdb_id, self.tmdb_id, self.trailer_url ) )
+            db.commit()
+            db.close()
+        except:
+            print "Failed to add to script Database"
+            
 Main()
